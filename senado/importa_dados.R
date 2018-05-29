64d37197c7b34cbbe7856c725de199e216c2b1de
Jair Neto <jair.neto@ccc.ufcg.edu.br>
2018-05-29 11:26:06 -0300

    Getting the bill's passage data using the Senate API

--- senado/importa_dados.R
+++ senado/importa_dados.R
@@ -2,14 +2,28 @@ install.packages("congressbr")
 library(congressbr)
 library(dplyr)
 
-bill_id <- 129808
-url_base <- "http://legis.senado.leg.br/dadosabertos/materia/votacoes/"
+#CongressBr
+#bill_id <- 91341
+#tramitacao_91341 <- sen_bills_passage(bill_id = bill_id) %>% filter(!is.na(bill_passage_text))
 
-tramitacao <- sen_bills_passage(bill_id = bill_id)
-tramitacao <- tramitacao %>% filter(!is.na(bill_passage_text))
 
-url <- paste(url_base, bill_id, sep = "")
-j <- jsonlite::fromJSON(url, flatten = T)
-df_identificacao_materia <- j$VotacaoMateria[4]$Materia$IdentificacaoMateria %>% purrr::map_df(~ .) 
-df_votacoes_geral <- j$VotacaoMateria[4]$Materia$Votacoes %>% purrr::map_df(~ .)
-df_votacoes_parlamentares <- tidyr::unnest(df_votacoes_geral)
+url_base_voting <- "http://legis.senado.leg.br/dadosabertos/materia/votacoes/"
+url_base_passage <- "http://legis.senado.leg.br/dadosabertos/materia/movimentacoes/"
+bill_id <- 91341
+
+#Voting data
+url <- paste(url_base_voting, bill_id, sep = "")
+json_voting <- jsonlite::fromJSON(url, flatten = T)
+list_bill_voting_data <- json_voting$VotacaoMateria[4]$Materia
+df_bill_voting_identification <- list_bill_voting_data$IdentificacaoMateria %>% purrr::map_df(~ .) 
+df_voting_general <- list_bill_voting_data$Votacoes %>% purrr::map_df(~ .)
+df_voting_parliamentarians <- tidyr::unnest(df_voting_general)
+
+#Passage Data
+url <- paste(url_base_passage, bill_id, sep = "")
+json_passage <- jsonlite::fromJSON(url, flatten = T)
+list_bill_passage_data <- json_passage$MovimentacaoMateria[4]$Materia
+df_bill_passage_identification <- list_bill_passage_data$IdentificacaoMateria %>% purrr::map_df(~ .)
+df_bill_actual_situation <- list_bill_passage_data$SituacaoAtual$Autuacoes$Autuacao$Situacao %>% purrr::map_df(~ .)
+df_bill_passages <- list_bill_passage_data$Tramitacoes %>% purrr::map_df(~ .)
+
