e293deec6a36e2f5f566d1437726f30a1c3096bf
jairNeto <jguedesf@uci.edu>
2018-05-31 11:39:02 -0300

    Adicionando função para importar dados básicos de uma proposição

--- code/senado-lib.R
+++ code/senado-lib.R
@@ -21,7 +21,8 @@ fetch_voting <- function(bill_id){
         map_df(~ .) %>% 
         unnest()
     
-    voting_df %>%
+    voting_df <-
+      voting_df %>%
         select(
             -c(
                 SessaoPlenaria.NomeCasaSessao,
@@ -39,6 +40,8 @@ fetch_voting <- function(bill_id){
             )
         ) %>% 
         add_column(!!! voting_ids)
+    
+    rename_voting_df(voting_df)
 }
 
 fetch_passage <- function(bill_id){
@@ -71,7 +74,8 @@ fetch_passage <- function(bill_id){
       extract2("Tramitacao") %>%
       as.tibble()
   
-    bill_passages_df %>%
+    bill_passages_df <-
+      bill_passages_df %>%
       select(
           -c(
              IdentificacaoTramitacao.OrigemTramitacao.Local.NomeCasaLocal,
@@ -84,5 +88,102 @@ fetch_passage <- function(bill_id){
             )
           ) %>%
           add_column(!!! passage_ids)
+    
+    rename_passage_df(bill_passages_df)
 }
+
+fetch_bill <- function(bill_id){
+  require(tidyverse)
+  require(magrittr)
+  library(jsonlite)
+  
+  url_base_bill <- "http://legis.senado.leg.br/dadosabertos/materia/"
+  
+  url <- paste0(url_base_bill, bill_id, sep = "")
+  json_bill <- fromJSON(url, flatten = T)
+  bill_data <- 
+    json_bill %>% 
+    extract2("DetalheMateria") %>% 
+    extract2("Materia")
+  bill_ids <-
+    bill_data %>% 
+    extract2("IdentificacaoMateria") %>% 
+    as.tibble()  %>%
+    select(CodigoMateria, SiglaSubtipoMateria, NumeroMateria)
+  bill_basic_data <-
+    bill_data %>%
+    extract2("DadosBasicosMateria") %>%
+    as.tibble()
+  bill_author <-
+    bill_data %>%
+    extract2("Autoria") %>%
+    extract2("Autor") %>%
+    as.tibble() %>%
+    select(NomeAutor, UfAutor, IdentificacaoParlamentar.CodigoParlamentar, IdentificacaoParlamentar.SiglaPartidoParlamentar)
+  bill_specific_subject <-
+    bill_data %>%
+    extract2("Assunto") %>% 
+    extract2("AssuntoEspecifico") %>%
+    as.tibble() %>%
+    rename(assunto_especifico = Descricao, codigo_assunto_especifico = Codigo)
+  bill_general_subject <-
+    bill_data %>%
+    extract2("Assunto") %>% 
+    extract2("AssuntoGeral") %>%
+    as.tibble() %>%
+    rename(assunto_geral = Descricao, codigo_assunto_geral = Codigo)
+  bill_source <-
+    bill_data %>%
+    extract2("OrigemMateria") %>%
+    as.tibble()
+  
+  bill_complete <- 
+    bill_basic_data %>%
+    add_column(!!! bill_ids, !!! bill_author, 
+               !!! bill_specific_subject, !!! bill_general_subject, !!! bill_source)
+  
+  rename_bill_df(bill_complete)
+}
+
+to_underscore <- function(x) {
+  x2 <- gsub("([A-Za-z])([A-Z])([a-z])", "\\1_\\2\\3", x)
+  x3 <- gsub(".", "_", x2, fixed = TRUE)
+  x4 <- gsub("([a-z])([A-Z])", "\\1_\\2", x3)
+  x5 <- tolower(x4)
+  x5
+}
+
+rename_voting_df <- function(df) {
+  new_names = names(df) %>% 
+    to_underscore() %>% 
+    str_replace("sessao_plenaria_|tramitacao_identificacao_tramitacao_|identificacao_parlamentar_", "")
+  
+  names(df) <- new_names
+  
+  df
+}
+
+rename_passage_df <- function(df) {
+  new_names = names(df) %>% 
+    to_underscore() %>% 
+    str_replace("identificacao_tramitacao_|
+                identificacao_tramitacao_origem_tramitacao_local_|
+                identificacao_tramitacao_destino_tramitacao_local_|
+                identificacao_tramitacao_situacao_", "")
+  
+  names(df) <- new_names
+  
+  df
+}
+
+rename_bill_df <- function(df) {
+  new_names = names(df) %>% 
+    to_underscore() %>% 
+    str_replace("identificacao_parlamentar_", "")
+  
+  names(df) <- new_names
+  
+  df
+}
+
 
--- senado/importa_dados.R
+++ senado/importa_dados.R
@@ -3,37 +3,19 @@ library(here)
 library(jsonlite)
 source(here("code/senado-lib.R"))
 
-to_underscore <- function(x) {
-  x2 <- gsub("([A-Za-z])([A-Z])([a-z])", "\\1_\\2\\3", x)
-  x3 <- gsub(".", "_", x2, fixed = TRUE)
-  x4 <- gsub("([a-z])([A-Z])", "\\1_\\2", x3)
-  x5 <- tolower(x4)
-  x5
-}
-
 bill_id <- 91341
 
 #Voting data
 voting <- fetch_voting(bill_id)
-new_names = names(voting) %>% 
-  to_underscore() %>% 
-  str_replace("sessao_plenaria_|tramitacao_identificacao_tramitacao_|identificacao_parlamentar_", "")
-
-names(voting) <- new_names
-
 voting %>% 
     write_csv(here(paste0("data/", bill_id, "-votacoes-senado.csv")))
 
 #Passage Data
 passage <- fetch_passage(bill_id)
-new_names = names(passage) %>% 
-  to_underscore() %>% 
-  str_replace("identificacao_tramitacao_|
-                   identificacao_tramitacao_origem_tramitacao_local_|
-                   identificacao_tramitacao_destino_tramitacao_local_|
-                   identificacao_tramitacao_situacao_", "")
-
-names(passage) <- new_names
-
 passage %>%
     write_csv(here(paste0("data/", bill_id, "-passage-senado.csv")))
+
+bill_data <- fetch_bill(bill_id)
+bill_data %>%
+  write_csv(here(paste0("data/", bill_id, "-bill-senado.csv")))
+
