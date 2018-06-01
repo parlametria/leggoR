a2005f9e9567e4f066b84d7d945af6cf3db0c2d0
jairNeto <jairguedesferreira@gmail.com>
2018-06-01 13:54:26 -0300

    Adicionando uma função para marcar os eventos de aprovação de audiencia publica, aprovação do parecer e aprovação do substitutivo

--- code/delimita-fases-Senado.R
+++ code/delimita-fases-Senado.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-bill_passage_91341 <- read.csv("91341-passage-senado.csv") %>% arrange(data_tramitacao)
+bill_passage_91341 <- read.csv("data/91341-passage-senado.csv") %>% arrange(data_tramitacao)
 
 phase_one <- c('^Este processo contém')
 phase_two <- 91
@@ -9,10 +9,10 @@ phase_four <- 52
 
 extract_phase <- function(dataframe) {
   dataframe <- dataframe %>%
-        mutate(fase = case_when( (grepl(phase_one, texto_tramitacao)) ~ 1,
-                                 (situacao_codigo_situacao == phase_two) ~ 2,
-                                 (situacao_codigo_situacao == phase_three) ~ 3,
-                                 (situacao_codigo_situacao == phase_four) ~ 4))
+        mutate(fase = case_when( (grepl(phase_one, texto_tramitacao)) ~ 'iniciativa',
+                                 (situacao_codigo_situacao == phase_two) ~ 'relatoria',
+                                 (situacao_codigo_situacao == phase_three) ~ 'discussao_deliberacao',
+                                 (situacao_codigo_situacao == phase_four) ~ 'virada_de_casa'))
 }
 
 replace_na_with_last <-function(x,a=!is.na(x)){
@@ -21,3 +21,16 @@ replace_na_with_last <-function(x,a=!is.na(x)){
 
 bill_passage_91341 <- extract_phase(bill_passage_91341) %>% arrange(data_tramitacao, numero_ordem_tramitacao)
 bill_passage_91341$fase <- replace_na_with_last(bill_passage_91341$fase)
+
+phase_aprovacao_audiencia <- 110
+phase_aprovacao_parecer <- 89
+phase_aprovacao_substitutivo <- 113
+
+extract_event <- function(dataframe) {
+  dataframe <- dataframe %>%
+    mutate(evento = case_when( (situacao_codigo_situacao == phase_aprovacao_audiencia) ~ 'aprovacao_audiencia_publica',
+                               (situacao_codigo_situacao == phase_aprovacao_parecer) ~ 'aprovacao_parecer',
+                               (situacao_codigo_situacao == phase_aprovacao_substitutivo) ~ 'aprovacao_substitutivo'))
+}
+
+bill_passage_91341 <- extract_event(bill_passage_91341)
