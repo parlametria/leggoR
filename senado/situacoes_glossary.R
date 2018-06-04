61f7326db5cc0c62e04758c7b2ecb57af6c91c61
Thayanne Sousa <thayanne@stormtrooper.analytics.ufcg.edu.br>
2018-06-04 14:58:13 -0300

    Add script de criação do glossário de situações

---
+++ senado/situacoes_glossary.R
@@ -0,0 +1,20 @@
+library(jsonlite)
+require(magrittr)
+
+url_situacoes_glossary <- "http://legis.senado.leg.br/dadosabertos/materia/situacoes.json"
+
+json_situacoes <- fromJSON(url_situacoes_glossary, flatten = T)
+
+#extract situacoes objects
+situacoes_data <-
+  json_situacoes %>%
+  extract2("ListaSituacoes") %>%
+  extract2("Situacoes")
+
+#transform into a data frame and rename headers
+situacoes <- as.data.frame(situacoes_data)
+situacoes$Situacao.DataCriacao <- NULL
+names(situacoes) <- c("codigo", "sigla", "descricao")
+
+#create file
+write.csv(situacoes, file = "data/situacoes-glossary-senado.csv",row.names=FALSE)
