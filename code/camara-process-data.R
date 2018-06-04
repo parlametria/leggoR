b41f54b111f69aa89ce8ebcfcc8c09ba6dbc12b9
hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-06-04 15:10:27 -0300

    Consertando inicio da fase 3

--- code/camara-process-data.R
+++ code/camara-process-data.R
@@ -8,7 +8,7 @@ tramitacao_pl_6726 <- fetch_tramitacao(id_prop = pl6726_id)
 
 phase_one <- c(100, 500)
 phase_two <- c(320)
-phase_three <- c(194)
+phase_three <- c(924)
 phase_four <- c(128)
 phase_five <- c(502, 251)
 
@@ -20,14 +20,14 @@ extract_phase <- function(dataframe) {
   dataframe %<>%
     mutate(fase = case_when(detect_phase(id_tipo_tramitacao, phase_one) ~ 'iniciativa',
                             detect_phase(id_tipo_tramitacao, phase_two) ~ 'relatoria',
-                            detect_phase(id_tipo_tramitacao, phase_three) ~ 'discussao_deliberacao',
+                            detect_phase(id_situacao, phase_three) ~ 'discussao_deliberacao',
                             detect_phase(id_tipo_tramitacao, phase_four) ~ 'virada_de_casa',
                             detect_phase(id_tipo_tramitacao, phase_five) ~ 'final',
                             detect_phase(id_situacao, 937) ~ 'final'))
 }
 
 to_underscore <- function(x) {
-  x %<>% 
+  x %<>%
     gsub("([A-Za-z])([A-Z])([a-z])", "\\1_\\2\\3", .) %>%
     gsub(".", "_", ., fixed = TRUE) %>%
     gsub("([a-z])([A-Z])", "\\1_\\2", .) %>%
