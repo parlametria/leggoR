5db3a3309811c434172c00a9603df1df033dc5ed
Juan Victor Lyra Barros e Barros <juanvlbb@kendall.analytics.ufcg.edu.br>
2018-06-05 17:59:43 -0300

    Organiza estados de fase

--- code/vis/tramitacao-senado.R
+++ code/vis/tramitacao-senado.R
@@ -1,19 +1,13 @@
 # SETUP
 install.packages("vistime")
 
-library("vistime")
-import_data <- read.csv(file="MyData.csv")
-import_data["color"] <- NA
+library(vistime)
+library(dplyr)
+library(purrr)
+library(lubridate)
+data <- read.csv("data/bill_passage_91341_visualization")
 
-import_data$color <- c(map(import_data$fase, function(fase) case_when(
-    fase == "iniciativa" ~ "#7fc97f",
-    fase == "relatoria" ~ "#beaed4",
-    fase == "discussao_deliberacao" ~ "#fdc086",
-    fase == "virada_de_casa" ~ "#ffff99"
-  ))
-)
-
-import_data <- import_data%>%
+data <- data %>%
   mutate(
     fase = case_when(
       fase == "iniciativa" ~ "Iniciativa",
@@ -23,9 +17,41 @@ import_data <- import_data%>%
     )
   )
 
+data_fixed <- data %>%
+  mutate(origem_tramitacao_local_sigla_local = as.character(origem_tramitacao_local_sigla_local)) %>%
+  mutate(group_local = 2) 
+  
+  
+data_fixed <- data_fixed %>% 
+                  rowwise() %>%
+                  do()
+    mutate(group_local = ifelse(lag(fase,default='NULL') != fase,
+                              lag(group_local,default=0) + 1,
+                              lag(group_local)))
+  #mutate(group_local = lag(origem_tramitacao_local_sigla_local,default='NULL'))
+  
+  #rowwise() %>%
+  
+
+
+
+
+
+
+
 datavis <- data.frame(Fase = import_data$fase,
-                   Events = import_data$texto_tramitacao,
-                   start = import_data$data_tramitacao,
-                   end = import_data$data_tramitacao,
-                   color = import_data$color)
-vistime(datavis, events="Events", groups="Fase", title="Fases da tramitação", colors="color", lineInterval=60*60*24*365, showLabels=FALSE)
+                      Events = import_data$evento,
+                      start = import_data$data_tramitacao,
+                      end = import_data$data_tramitacao)
+vistime(datavis, events="Events", groups="Fase", title="Fases da tramitação", lineInterval=60*60*24*365, showLabels=FALSE)
+
+
+
+
+######################### TARGET ############################################
+
+tramitacao_data <- data.frame(level = rep("LOCAL",4),
+                              start = c("2017-12-3", "2017-12-4", "2018-3-5", "2018-4-12"),
+                              end = c("2017-12-4", "2018-3-5", "2018-4-12", "2018-4-13"),
+                              event = c("SEC", "CCJ", "CAE", "PLEN"))
+vistime(tramitacao_data, events = "event", groups="local")
