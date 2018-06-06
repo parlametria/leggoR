831fb9362722409bd7303c355c725568e90c8972
Juan Victor Lyra Barros e Barros <juanvlbb@kendall.analytics.ufcg.edu.br>
2018-06-06 11:31:17 -0300

    Cria visualização de fases

--- code/vis/tramitacao-senado.R
+++ code/vis/tramitacao-senado.R
@@ -1,49 +1,47 @@
 # SETUP
-install.packages("vistime")
+# install.packages("vistime")
 
 library(vistime)
 library(dplyr)
 library(purrr)
-library(lubridate)
 data <- read.csv("data/bill_passage_91341_visualization")
 
-data <- data %>%
-  mutate(
-    fase = case_when(
-      fase == "iniciativa" ~ "Iniciativa",
-      fase == "relatoria" ~ "Relatoria",
-      fase == "discussao_deliberacao" ~ "Discussão e Deliberação",
-      fase == "virada_de_casa" ~ "Troca de Casa"
+# Create data frame to display phases inline
+format_phase_data <- function(df) {
+  df <- 
+    # Improve the phases names and convert data_tramitacao to Date
+    df %>%
+    mutate(
+      # fase = case_when(
+      #   fase == "iniciativa" ~ "Iniciativa",
+      #   fase == "relatoria" ~ "Relatoria",
+      #   fase == "discussao_deliberacao" ~ "Discussão e Deliberação",
+      #   fase == "virada_de_casa" ~ "Troca de Casa"
+      # ),
+      data_tramitacao = as.Date(data_tramitacao),
+      local = as.character(origem_tramitacao_local_sigla_local)
     )
-  )
 
-data_fixed <- data %>%
-  mutate(origem_tramitacao_local_sigla_local = as.character(origem_tramitacao_local_sigla_local)) %>%
-  mutate(group_local = 2) 
-  
-  
-data_fixed <- data_fixed %>% 
-                  rowwise() %>%
-                  do()
-    mutate(group_local = ifelse(lag(fase,default='NULL') != fase,
-                              lag(group_local,default=0) + 1,
-                              lag(group_local)))
-  #mutate(group_local = lag(origem_tramitacao_local_sigla_local,default='NULL'))
-  
-  #rowwise() %>%
-  
+    df %>%
+      mutate(z = cumsum(local != lag(local, default='NULL'))) %>%
+      group_by(local, z) %>%
+      summarize(start = min(data_tramitacao),
+                end = max(data_tramitacao) + 1,
+                time_interval = end - start) #%>%
+      # mutate(
+      #   line = "Local",
+      #   color = case_when(
+      #     fase == "Iniciativa" ~ "#7fc97f",
+      #     fase == "Relatoria" ~ "#beaed4",
+      #     fase == "Discussão e Deliberação" ~ "#fdc086",
+      #     fase == "Troca de Casa" ~ "#ffff99"
+      #   ))
+}
 
+fase_rows <- format_phase_data(data)
 
-
-
-
-
-
-datavis <- data.frame(Fase = import_data$fase,
-                      Events = import_data$evento,
-                      start = import_data$data_tramitacao,
-                      end = import_data$data_tramitacao)
-vistime(datavis, events="Events", groups="Fase", title="Fases da tramitação", lineInterval=60*60*24*365, showLabels=FALSE)
+# Create chart with all tasks.
+vistime(fase_rows, events="local", colors="color", groups="line", title="Fases da tramitação", showLabels=FALSE)
 
 
 
@@ -55,3 +53,4 @@ tramitacao_data <- data.frame(level = rep("LOCAL",4),
                               end = c("2017-12-4", "2018-3-5", "2018-4-12", "2018-4-13"),
                               event = c("SEC", "CCJ", "CAE", "PLEN"))
 vistime(tramitacao_data, events = "event", groups="local")
+
