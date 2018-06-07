f93b7ed2433503ac1fbab2e4790bc1529a4b4610
hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-06-07 10:57:35 -0300

    Criada função que extrai local para visualização em timeline

--- code/vis/tramitacao/data-chart-camara.R
+++ code/vis/tramitacao/data-chart-camara.R
@@ -1,8 +1,8 @@
+library(data.table)
+library(dplyr)
+library(lubridate)
+
 data_fase <- function(df) {
-  require(data.table)
-  require(dplyr)
-  require(lubridate)
-    
   df %<>%
     mutate(end_data = lead(data_hora)) %>% 
     group_by(fase, sequence = rleid(fase)) %>%
@@ -23,3 +23,22 @@ data_fase <- function(df) {
   df
 }
 
+data_local <- function(df) {
+  df %<>% 
+    mutate(end_data = lead(data_hora)) %>%
+    group_by(sigla_orgao, sequence = rleid(sigla_orgao)) %>%
+    summarise(start = min(data_hora),
+              end = max(end_data)) %>%
+    ungroup() %>%
+    arrange(sequence) %>% select(-sequence) %>%
+    setnames("sigla_orgao", "label") %>%
+    mutate(group = "Local",
+           color = case_when(label == "iniciativa" ~ "#7fc97f",
+                             label == "relatoria" ~ "#fdc086",
+                             label == "discussao_deliberacao" ~ "#beaed4",
+                             label == "virada_de_casa" ~ "#ffff99",
+                             label == "final" ~ "#f4fa58"))
+  
+  df$end[nrow(df)] <- ymd(Sys.Date())
+  df
+}
