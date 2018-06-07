310f7563918273874e11261ebfd4af9ea75650bb
AndresMRM <andres@inventati.org>
2018-06-07 17:24:14 -0300

    simplifica funções que geram tabela usada pela timeline

--- code/vis/tramitacao/data-chart-camara.R
+++ code/vis/tramitacao/data-chart-camara.R
@@ -9,8 +9,8 @@ data_path <- here::here('data/vis/tramitacao/')
 file_path <- paste(c(data_path,'data-camara.csv'),  collapse = '')
 
 data_fase <- function(df) {
-  df %<>%
-    mutate(end_data = lead(data_hora)) %>% 
+  df %>%
+    mutate(end_data = lead(data_hora, default=Sys.time())) %>% 
     group_by(fase, sequence = rleid(fase)) %>%
       summarise(start = min(data_hora),
               end = max(end_data)) %>% 
@@ -24,20 +24,18 @@ data_fase <- function(df) {
                              label == "discussao_deliberacao" ~ "#beaed4",
                              label == "virada_de_casa" ~ "#ffff99",
                              label == "final" ~ "#f4fa58"))
-  
-  df$end[nrow(df)] <- ymd(Sys.Date())
-  df
 }
 
 data_local <- function(df) {
-  df %<>% 
-    mutate(end_data = lead(data_hora)) %>%
+  df %>% 
+    mutate(end_data = lead(data_hora, default=Sys.time())) %>%
     group_by(sigla_orgao, sequence = rleid(sigla_orgao)) %>%
     summarise(start = min(data_hora),
               end = max(end_data)) %>%
     filter(end - start > 0) %>%
     ungroup() %>%
-    arrange(sequence) %>% select(-sequence) %>%
+    arrange(sequence) %>%
+    select(-sequence) %>%
     rename(label = sigla_orgao) %>%
     mutate(group = "Local",
            color = case_when(label == "MESA" ~ "#E2E3D9",
@@ -46,22 +44,17 @@ data_local <- function(df) {
                              label == "CCP" ~ "#8bca42",
                              label == "SEPRO" ~ "#ca4242",
                              label == "CTASP" ~ "#ea81b1"))
-  
-  df$end[nrow(df)] <- ymd(Sys.Date())
-  df
 }
 
 data_evento <- function(df) {
-  df %<>%
+  df %>%
     filter(!is.na(evento)) %>% 
     mutate(start = data_hora, end = data_hora, group = 'Evento') %>% 
     group_by(evento) %>%
     rename(label = evento) %>%
     select(label, start, end, group) %>%
     mutate(color = '#a9a9a9')
-  df
 }
 
 data <- bind_rows(data_evento(tramitacao), data_fase(tramitacao), data_local(tramitacao))
 readr::write_csv(data, file_path)
-
