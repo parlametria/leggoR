b95534f78a35a3c24b361da4cee7563a46838932
hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-06-11 17:41:46 -0300

    Criada função que pega uma proposicao do rcongresso e renomeia as colunas

--- code/camara-process-data.R
+++ code/camara-process-data.R
@@ -14,6 +14,7 @@ pl6726_id <- fetch_id_proposicao(tipo = "PL", numero = 6726, ano = 2016)
 tramitacao_pl_6726 <- fetch_tramitacao(id_prop = pl6726_id)
 
 csv_path <- paste(c(data_path,'tramitacao_camara_', pl6726_id, '.csv'),  collapse = '')
+proposicao_csv_path <- paste(c(data_path,'proposicao_camara_', pl6726_id, '.csv'),  collapse = '')
 
 phase_one <- c(100, 500)
 phase_two <- c(320)
@@ -98,4 +99,16 @@ tramitacao_pl_6726$evento %>%
   as.data.frame %>%
   arrange(desc(Freq)) %>%
   htmlTable(header=c('evento', 'frequência'), rnames=FALSE)
+
+#Fetch a bill with renamed columns
+fetch_proposicao_renamed <- function(id) {
+  df <-
+    fetch_proposicao(pl6726_id) %>%
+    rename_df_columns
+  df
+}
+
+proposicao_pl_6726 <- 
+  fetch_proposicao_renamed(pl6726_id) %>%
+  readr::write_csv(proposicao_csv_path)
 
