de09ec54fba138078b7ce5cad5f6cadb715f5a41
Daniel Rodrigues Coura <danielrc@regressao.analytics.ufcg.edu.br>
2018-06-11 10:03:17 -0300

    Adiciona função que retorna a última etapa da tramitacao da Câmara e do Senado

--- code/camara-lib.R
+++ code/camara-lib.R
@@ -4,3 +4,6 @@ get_nome_ementa_Camara <- function(bill_id) {
   fetch_proposicao(pl6726_id) %>% select(ementa, siglaTipo, numero)
 }
 
+tail_descricao_despacho_Camara <- function(df, qtd=1) {
+  df[(nrow(df)-(qtd-1)):nrow(df), c('data_hora', 'descricao_tramitacao', 'despacho')]
+}
