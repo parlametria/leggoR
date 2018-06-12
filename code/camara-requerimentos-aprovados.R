89ab08514d497ebf6a15ffad14e8283f11955b87
AndresMRM <andres@inventati.org>
2018-06-12 17:20:47 -0300

    adiciona código teste para marcar requerimentos como deferidos ou não

---
+++ code/camara-requerimentos-aprovados.R
@@ -0,0 +1,41 @@
+library(rcongresso)
+library(tidyverse)
+library(dplyr)
+library(magrittr)
+library(fuzzyjoin)
+
+
+regexes <- 
+  frame_data(~ status, ~ regex,
+           "indeferido", '^Indefiro',
+           "deferido", '^Defiro')
+
+
+relacionadas <- 
+  fetch_relacionadas(46249)$uri %>% 
+  strsplit('/') %>% 
+  vapply(last, '') %>% 
+  unique %>% 
+  fetch_proposicao
+
+requerimentos <- 
+  relacionadas %>% 
+  filter(str_detect(.$descricaoTipo, '^Requerimento'))
+
+tramitacoes <- 
+  requerimentos$id %>% 
+  fetch_tramitacao
+
+relacionadas <-
+  tramitacoes %>% 
+  # mark tramitacoes rows based on regexes
+  regex_left_join(regexes, by=c(despacho="regex")) %>% 
+  group_by(id_prop) %>% 
+  # fill down marks
+  fill(status) %>%
+  # get last mark on each tramitacao
+  do(tail(., n=1)) %>%
+  ungroup %>% 
+  select(id_prop, status) %>% 
+  # and mark proposicoes based on last tramitacao mark
+  left_join(relacionadas, by=c('id_prop' = 'id'))
