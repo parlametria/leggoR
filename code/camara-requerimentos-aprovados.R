c90390482d05ed1472b0afaf44bcec4aa797f084
AndresMRM <andres@inventati.org>
2018-06-12 18:09:00 -0300

    nomeia função para pegar requerimentos

--- code/camara-requerimentos-aprovados.R
+++ code/camara-requerimentos-aprovados.R
@@ -10,32 +10,36 @@ regexes <-
            "indeferido", '^Indefiro',
            "deferido", '^Defiro')
 
+fetch_requerimentos <- function(id, mark_status=TRUE) {
+  relacionadas <- 
+    fetch_relacionadas(id)$uri %>% 
+    strsplit('/') %>% 
+    vapply(last, '') %>% 
+    unique %>% 
+    fetch_proposicao
+  
+  requerimentos <- 
+    relacionadas %>% 
+    filter(str_detect(.$descricaoTipo, '^Requerimento'))
+  
+  if(!mark_status) return(requerimentos)
+  
+  tramitacoes <- 
+    requerimentos$id %>% 
+    fetch_tramitacao
+  
+  relacionadas <-
+    tramitacoes %>% 
+    # mark tramitacoes rows based on regexes
+    regex_left_join(regexes, by=c(despacho="regex")) %>% 
+    group_by(id_prop) %>% 
+    # fill down marks
+    fill(status) %>%
+    # get last mark on each tramitacao
+    do(tail(., n=1)) %>%
+    ungroup %>% 
+    select(id_prop, status) %>% 
+    # and mark proposicoes based on last tramitacao mark
+    left_join(relacionadas, by=c('id_prop' = 'id'))
+}
 
-relacionadas <- 
-  fetch_relacionadas(46249)$uri %>% 
-  strsplit('/') %>% 
-  vapply(last, '') %>% 
-  unique %>% 
-  fetch_proposicao
-
-requerimentos <- 
-  relacionadas %>% 
-  filter(str_detect(.$descricaoTipo, '^Requerimento'))
-
-tramitacoes <- 
-  requerimentos$id %>% 
-  fetch_tramitacao
-
-relacionadas <-
-  tramitacoes %>% 
-  # mark tramitacoes rows based on regexes
-  regex_left_join(regexes, by=c(despacho="regex")) %>% 
-  group_by(id_prop) %>% 
-  # fill down marks
-  fill(status) %>%
-  # get last mark on each tramitacao
-  do(tail(., n=1)) %>%
-  ungroup %>% 
-  select(id_prop, status) %>% 
-  # and mark proposicoes based on last tramitacao mark
-  left_join(relacionadas, by=c('id_prop' = 'id'))
