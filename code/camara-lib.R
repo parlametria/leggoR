84632df3e2bb651d43cfeb6954cf39e46f154c99
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-06-13 15:29:04 -0300

    Adciona documentação da camara-lib

--- code/camara-lib.R
+++ code/camara-lib.R
@@ -1,6 +1,13 @@
 library(here)
 source(here("code/congresso-lib.R"))
 
+#' @title Recupera o número, o tipo e ementa de uma proposição na Câmara
+#' @description Retorna um dataframe contendo o número, o tipo e a ementa de uma proposição na Câmara através do ID da proposição
+#' @param bill_id ID da proposição
+#' @return Dataframe com o número, o tipo e a ementa da proposição na Câmara.
+#' @examples
+#' get_nome_ementa_Camara(2121442)
+#' @export
 get_nome_ementa_Camara <- function(bill_id) {
   require(dplyr)
   require(rcongresso)
@@ -8,6 +15,15 @@ get_nome_ementa_Camara <- function(bill_id) {
   fetch_proposicao(bill_id) %>% select(ementa, siglaTipo, numero)
 }
 
+#' @title Recupera os n últimos despachos na Câmara
+#' @description Retorna um dataframe das últimas n tramitações na Câmara contendo a hora, a descrição e o despacho 
+#' @param df Dataframe da tramitação na Câmara
+#' @param qtd  (opcional) Quantidade de eventos a serem recuperados
+#' @return Dataframe com as última n tramitações da Câmara.
+#' @examples
+#' tramitacao %>% tail_descricao_despacho_Camara()
+#' tramitacao %>% tail_descricao_despacho_Camara(4)
+#' @export
 tail_descricao_despacho_Camara <- function(df, qtd=1) {
   require(dplyr)
   
@@ -17,6 +33,14 @@ tail_descricao_despacho_Camara <- function(df, qtd=1) {
     select(data_hora, descricao_tramitacao, despacho)
 }
 
+#' @title Cria coluna com os relatores na tramitação na Câmara
+#' @description Cria uma nova coluna com os relatores na Câmara. O relator é adicionado à coluna no 
+#' envento pontual em que ele é designado
+#' @param df Dataframe da tramitação na Câmara
+#' @return Dataframe com a coluna "relator" adicionada.
+#' @examples
+#' tramitacao %>% extract_relator_Camara()
+#' @export
 extract_relator_Camara <- function(df) {
   require(dplyr)
   require(stringr)
@@ -26,6 +50,13 @@ extract_relator_Camara <- function(df) {
              case_when(str_detect(tolower(despacho), '^designad. relat.r') ~ str_extract(despacho, regex('dep.+', ignore_case=TRUE))))
 }
 
+#' @title Cria coluna com as fases da tramitação na Câmara
+#' @description Cria uma nova coluna com as fases na Câmara.
+#' @param df Dataframe da tramitação na Câmara
+#' @return Dataframe com a coluna "fase" adicionada.
+#' @examples
+#' tramitacao %>% extract_phases_Camara()
+#' @export
 extract_phases_Camara <- function(dataframe, phase_one, phase_two, phase_three, phase_four, phase_five) {
   require(magrittr)
   
@@ -38,7 +69,15 @@ extract_phases_Camara <- function(dataframe, phase_one, phase_two, phase_three, 
                             detect_phase(id_situacao, 937) ~ 'final'))
 }
 
-extract_n_last_events_Camara <- function(df, num) {
+#' @title Busca os últimos n eventos da tramitação na Câmara
+#' @description Recupera os útimos n eventos da tramitação na Cãmara, caso nenhuma quantidade seja informada, assume-se que é 1
+#' @param df Dataframe da tramitação na Câmara
+#' @return Dataframe dos últimos n eventos na Câmara contendo hora e evento.
+#' @examples
+#' tramitacao %>% extract_n_last_events_Camara()
+#' tramitacao %>% extract_n_last_events_Camara(3)
+#' @export
+extract_n_last_events_Camara <- function(df, num=1) {
   require(tidyverse)
   
   df %>%
@@ -47,6 +86,13 @@ extract_n_last_events_Camara <- function(df, num) {
     select(data_hora, evento)
 }
 
+#' @title Renomeia as colunas do dataframe
+#' @description Renomeia as colunas do dataframe usando o padrão de letras minúsculas e underscore
+#' @param df Dataframe 
+#' @return Dataframe com as colunas renomeadas.
+#' @examples
+#' df %>% rename_df_columns()
+#' @export
 rename_df_columns <- function(df) {
   require(magrittr)
   
@@ -54,6 +100,14 @@ rename_df_columns <- function(df) {
   df
 }
 
+#' @title Extrai os eventos importantes que aconteceram na Câmara
+#' @description Adiciona coluna ao dataframe com os eventos mais importantes que aconteceram na Câmara
+#' @param tramitacao_df Dataframe da tramitação na Cãmara
+#' @param events_df Dataframe com os eventos contendo as colunas "evento" e "regex"
+#' @return Dataframe com a coluna "evento" adicionada.
+#' @examples
+#' df %>% extract_events_Camara(importants_events)
+#' @export
 extract_events_Camara <- function(tramitacao_df, events_df) {
   require(magrittr)
   require(tidyverse)
@@ -67,19 +121,39 @@ extract_events_Camara <- function(tramitacao_df, events_df) {
                               TRUE ~ evento))
 }
 
+#' @title Altera as datas da tramitação em um formato mais fácil de tratar 
+#' @description Refatora cada data da coluna para o formato POSIXct
+#' @param df Dataframe da tramitação na Cãmara
+#' @return Dataframe com a coluna de datas refatorada para um formato tratável.
+#' @examples
+#' df %>% refact_date()
+#' @export
 refact_date <- function(df) {
   require(lubridate)
   
   mutate(df, data_hora = ymd_hm(data_hora))
 }
 
-# sort the 'tramitacao' dataframe by date
+#' @title Ordena o dataframe de acordo com a data
+#' @description Ordena o dataframe de acordo com a data
+#' @param df Dataframe contendo coluna de datas
+#' @return Dataframe ordenado pela data
+#' @examples
+#' df %>% sort_by_date()
+#' @export
 sort_by_date <- function(df) {
   require(tidyverse)
   
   arrange(df, data_hora, sequencia)
 }
 
+#' @title Recupera o autor de uma proposição na Câmara
+#' @description Retorna um dataframe contendo o link, o nome, o código do tipo, o tipo e a casa de origem do autor
+#' @param prop_id ID da proposição
+#' @return Dataframe contendo o link, o nome, o código do tipo, o tipo e a casa de origem do autor.
+#' @examples
+#' extract_autor_Camara(2121442)
+#' @export
 extract_autor_Camara <- function(prop_id) {
   require(magrittr)
   require(jsonlite)
@@ -105,7 +179,13 @@ extract_autor_Camara <- function(prop_id) {
   autores
 }
 
-# Retorna a lista de IDs das proposições apensadas
+#' @title Recupera as proposições apensadas
+#' @description Retorna os IDs das proposições apensadas a uma determinada proposição
+#' @param prop_id ID da proposição
+#' @return Ventor contendo os IDs das proposições apensadas
+#' @examples
+#' fetch_apensadas(2121442)
+#' @export
 fetch_apensadas <- function(prop_id) {
   require(xml2)
   api_v1_proposicao = 'http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterProposicaoPorID?IdProp='
