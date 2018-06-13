1bb5513b4b74a5253b619c650608600c482b4b4c
Juan Victor Lyra Barros e Barros <juanvlbb@boxplot.analytics.ufcg.edu.br>
2018-06-13 15:55:33 -0300

    Executando script de importação do senado

---
+++ code/vis/tramitacao/build-data.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+ 
+# Get the arguments list 
+cmdargs = str(sys.argv)
+ 
+# Print it
+if len(sys.argv) == 3:
+    id_project = sys.argv[1]
+    house = str(sys.argv[2])
+    if house.upper() == "SENADO":
+        subprocess.call ("Rscript --vanilla ../../importa-dados-Senado.R {}".format(id_project), shell=True)
+    elif house.upper() == "CAMARA":
+        print("CAMARA")
+    else:
+        print("Wrong parameter: House just can be 'Camara' or 'Senado'")
+else:
+    print("Wrong number of parameters")
+    print("Usage:")
+    print("./build-data.py <id_project> <house>")
