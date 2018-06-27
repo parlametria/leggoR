e43fcb93858a7ce3603b847edfcda95e6639f897
AndresMRM <andres@inventati.org>
2018-06-27 08:21:19 -0300

    melhora teste

--- tests/testthat/test-camara-lib.R
+++ tests/testthat/test-camara-lib.R
@@ -1,8 +1,10 @@
 context('test-camara-lib.R')
 
-test_that('fetch proposicao funciona', {
+test_that('fetch_proposicao retorna uma row por id', {
+  props <- camara_fetch_proposicao(2056568)
+  expect_equal(nrow(props), 1)
+
   prop_id <- c(257161, 345311)
-  ## props <- agoradigital::fetch_proposicao(prop_id)
   props <- camara_fetch_proposicao(prop_id)
   expect_equal(nrow(props), 2)
 })
