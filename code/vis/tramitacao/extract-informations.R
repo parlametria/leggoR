54db4b4ebff8408b852458dd09a4364f1a53ff8c
Jair Guedes <jair@spearman.analytics.ufcg.edu.br>
2018-06-28 14:07:49 -0300

    Adiciona partido e estado dos autores na visualização da proposição

--- code/vis/tramitacao/extract-informations.R
+++ code/vis/tramitacao/extract-informations.R
@@ -86,6 +86,9 @@ extract_informations_from_single_house <- function(id, casa, url=NULL) {
     nome <- paste0(nome_senado$sigla_subtipo_materia, nome_senado$numero_materia)
     casa_origem <- proposicao$nome_casa_origem
     nome_autor <- proposicao$nome_autor
+    partido_autor <- proposicao$sigla_partido_parlamentar
+    uf_autor <- proposicao$uf_parlamentar
+    nome_autor <- paste0(nome_autor, " ", partido_autor, "/", uf_autor)
     despacho <- despacho_senado$texto_tramitacao
     relatoria <- fetch_last_relatoria(id) %>% tail(1)
     relator <- as.character(relatoria$nome_parlamentar)
