69d32bf2b4fd8e08c24672a647bdf2f667b36446
thayannevls <thayannevls@gmail.com>
2018-06-28 15:05:40 -0300

    cria estrutura de teste em camara-lib

--- tests/testthat/test-camara-lib.R
+++ tests/testthat/test-camara-lib.R
@@ -1,10 +1,34 @@
-context('test-camara-lib.R')
+source('test-camara-lib.R')
 
-test_that('fetch_proposicao retorna uma row por id', {
-  props <- camara_fetch_proposicao(2056568)
-  expect_equal(nrow(props), 1)
+setup <- function(){
+  
+  #PL 3729/2004 e PL 490/2007
+  PROPOSICOES_ID <<- c(2121442, 345311)
+  TAM_LISTA_PROPOSICOES <<- 4
+  
+  proposicoes_dados <<- fetch_proposicao(PROPOSICOES_ID)
+  
+  return(TRUE)
+}
 
-  prop_id <- c(257161, 345311)
-  props <- camara_fetch_proposicao(prop_id)
-  expect_equal(nrow(props), 2)
-})
+check_script <- function(){
+  tryCatch(setup(), error = function(e){return(FALSE)})
+}
+
+test <- function(){
+  
+  context("Testing integrity data")
+  
+  test_that('fetch_proposicao retorna uma row por id', {
+    props <- camara_fetch_proposicao(2056568)
+    expect_equal(nrow(props), 1)
+    
+    prop_id <- c(257161, 345311)
+    props <- camara_fetch_proposicao(prop_id)
+    expect_equal(nrow(props), 2)
+  })
+}
+
+if(check_script()){
+  test()
+} else skip()
