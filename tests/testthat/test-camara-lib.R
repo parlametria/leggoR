b882eb7649616f50ccb37f6e44337dbeeab56af9
thayannevls <thayannevls@gmail.com>
2018-06-29 14:15:16 -0300

    configura estrutura de testes

--- tests/testthat/test-camara-lib.R
+++ tests/testthat/test-camara-lib.R
@@ -1,13 +1,13 @@
-source('test-camara-lib.R')
+context('test-camara-lib.R')
 
 setup <- function(){
-  
+
   #PL 3729/2004 e PL 490/2007
   PROPOSICOES_ID <<- c(2121442, 345311)
   TAM_LISTA_PROPOSICOES <<- 2
-  
-  proposicoes_dados <<- fetch_proposicao(PROPOSICOES_ID)
-  
+
+  proposicoes_dados <<- camara_fetch_proposicao(PROPOSICOES_ID)
+
   return(TRUE)
 }
 
@@ -16,14 +16,13 @@ check_script <- function(){
 }
 
 test <- function(){
-  
-  context("Testing data integrity")
-  
-  test_that('fetch_proposicao retorna uma row por id', {
+
+  test_that('fetch_proposicao() returns one row per id', {
     props <- camara_fetch_proposicao(PROPOSICOES_ID)
     expect_equal(nrow(props), TAM_LISTA_PROPOSICOES)
   })
   
+  test_that('fetch_proposicao()')
 }
 
 if(check_script()){
