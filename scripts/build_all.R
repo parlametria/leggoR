eca8009c0285df1bec5652c62997bc97ff4903cf
AndresMRM <andres@inventati.org>
2018-07-16 09:54:19 -0300

    usa html_document ao invés de html_notebook tentando evitar travamentos

--- scripts/build_all.R
+++ scripts/build_all.R
@@ -6,6 +6,8 @@ library(here)
 source(here::here('scripts/vis/tramitacao/build-data.R'))
 source(here::here('scripts/renderReport.R'))
 
+output_dir = here::here('docs/reports')
+
 # Create dirs when needed
 c('data/Senado', 'data/camara', 'data/vis/tramitacao', 'docs/reports') %>%
   lapply(dir.create, recursive=TRUE, showWarnings = FALSE)
@@ -16,9 +18,10 @@ readr::read_csv('data/tabela_geral_ids_casa.csv') %>% build_all_csvs()
 # Build reports
 render_all_reports()
 
-# Build reports table
-render(
-  here::here('reports/tabela-proposicoes.Rmd'),
-  'html_notebook',
-  output_dir = output_dir,
-)
+# Build tabela e gabaritos
+c(
+  'reports/tabela-proposicoes.Rmd',
+  'reports/gabarito/pl-3729-2004-timeline.Rmd',
+  'reports/gabarito/pl-490-2007-timeline.Rmd'
+) %>%
+  {lapply(here::here(.), render, 'html_document', output_dir = output_dir)}
