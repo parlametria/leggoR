cb137b0062e74dad3e206293fae463cb6d7b5f2c
Jair Guedes <jairguedesferreira@gmail.com>
2018-07-20 10:09:10 -0300

    Tira fase virada de casa de comissão

--- scripts/vis/tramitacao/data-chart-senado.R
+++ scripts/vis/tramitacao/data-chart-senado.R
@@ -68,8 +68,7 @@ format_eventos <- function(df) {
       evento = as.character(evento)
     )
   
-  df <- 
-    df %>%
+  df %>%
     mutate(z = cumsum(evento != lag(evento, default='NULL')),
            end_data = lead(data_tramitacao)) %>%
     group_by(evento, sequence = data.table::rleid(z)) %>%
@@ -85,48 +84,22 @@ format_eventos <- function(df) {
   
 }
 
-#Create data to display global phase inlines
-create_fase_global <- function(data_tramitacao, bill_id) {
-  data_prop <- read_csv(paste0(here::here("data/Senado/"), bill_id,"-proposicao-senado.csv"))
-  casa_origem <- if_else(data_prop$nome_casa_origem == "Senado Federal", " - Casa de Origem (Senado)", " - Casa Revisora (Câmara)")
-  
-  virada_de_casa <- 
-    data_tramitacao %>%
-    filter(fase == "Virada de Casa") %>%
-    arrange(data_tramitacao) %>%
-    select(data_tramitacao)
-  
-  if(nrow(virada_de_casa) == 0) {
-    data_tramitacao %>%
-      mutate(casa = paste0(casa, casa_origem))
-  }else {
-    casa_atual <- if_else(casa_origem == " - Casa de Origem (Senado)", " - Casa Revisora (Câmara)", " - Casa de Origem (Senado)")
-    data_tramitacao %>%
-      mutate(casa = if_else(data_tramitacao < virada_de_casa[1, ][[1]], paste0(casa, casa_origem), paste0(casa, casa_atual)))
-  }
-}
-
 #Create data to display casa inlines
 format_fase_global <- function(df) {
-  df <-
-    # Improve the phases names and convert data_tramitacao to Date
-    df %>%
-    mutate(
-      data_tramitacao = as.Date(data_tramitacao)
-    )
   
   df %>%
-    mutate(z = cumsum(casa != lag(casa, default='NULL')),
+    mutate(global = paste0(casa, " ", global)) %>%
+    mutate(z = cumsum(global != lag(global, default='NULL')),
            end_data = lead(data_tramitacao)) %>%
-    group_by(casa, sequence = data.table::rleid(z)) %>%
+    group_by(global, sequence = data.table::rleid(z)) %>%
     summarize(start = min(data_tramitacao),
               end = if_else(is.na(max(end_data)),max(data_tramitacao),max(end_data)),
               time_interval = end - start) %>%
     ungroup() %>%
     arrange(sequence) %>%
     select(-sequence) %>%
-    filter(!is.na(casa)) %>%
-    rename(label=casa) %>%
+    filter(!is.na(global)) %>%
+    rename(label=global) %>%
     mutate(group = "Global",
            color = case_when(stringr::str_detect(label, "Plenário") ~ "#5496cf",
                              stringr::str_detect(label, "Comissões") ~ "#938ecc",
@@ -137,12 +110,12 @@ format_fase_global <- function(df) {
 
 build_vis_csv <- function(bill_id) {
   data_tramitacao <- 
-    read_csv(paste0(here::here("data/Senado/"), bill_id,"-visualizacao-tramitacao-senado.csv")) %>%
-    create_fase_global(bill_id)
+    read_csv(paste0(here::here("data/Senado/"), bill_id,"-visualizacao-tramitacao-senado.csv"))
+  
   df <- 
     rbind(format_fase_global(data_tramitacao),
-              format_local(data_tramitacao), 
-              format_fase(data_tramitacao),
-              format_eventos(data_tramitacao))  %>%
+          format_local(data_tramitacao), 
+          format_fase(data_tramitacao),
+          format_eventos(data_tramitacao))  %>%
     write_csv(paste0(here::here("data/vis/tramitacao/"), bill_id, "-data-senado.csv"))
 }
