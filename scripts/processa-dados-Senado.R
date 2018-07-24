7a3c560877ab9f103314cef16ce2022a7ebad57e
Jair Guedes <jairguedesferreira@gmail.com>
2018-07-24 08:35:41 -0300

    Ajeita local no Senado

--- scripts/processa-dados-Senado.R
+++ scripts/processa-dados-Senado.R
@@ -21,8 +21,7 @@ process_proposicao <- function(bill_id){
   bill_passage <- 
     extract_fase_Senado(bill_passage, phase_one, recebimento_phase, phase_two, phase_three, encaminhamento_phase, phase_four) %>% 
     arrange(data_tramitacao, numero_ordem_tramitacao) %>%
-    fill(fase) %>%
-    filter(!is.na(fase))
+    fill(fase) 
 
   bill_passage$situacao_descricao_situacao <- 
     to_underscore(bill_passage$situacao_descricao_situacao) %>% 
@@ -49,7 +48,8 @@ process_proposicao <- function(bill_id){
   bill_passage <- 
     bill_passage[1:index_of_camara, ] %>% 
     extract_locais() %>%
-    extract_fase_global(bill_id)
+    extract_fase_global(bill_id) %>%
+    filter(!is.na(fase))
   
   bill_passage %>%
     write_csv(paste0(here::here("data/Senado/"), bill_id, "-fases-tramitacao-senado.csv"))
