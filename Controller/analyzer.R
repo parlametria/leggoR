63363fd3a51240f6fdd6663ddbdb25f6428f9275
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2018-08-03 11:27:09 -0300

    Adicionada extração do número do requerimento aprovado

--- Controller/analyzer.R
+++ Controller/analyzer.R
@@ -147,12 +147,18 @@ extract_evento_in_camara <- function(df) {
                     tolower(descricao_tramitacao) == redistribuicao_text ~ "redistribuicao"))
 }
 extract_approved_requerimentos_in_senado <- function(df) {
-  apr_requerimentos_regex <- c('(aprovad.* | deferid.*)requeriment.*')
+  apr_requerimentos_regex <- c('(aprova.* | deferid.*)requeriment.* nº.*')
   apr_requerimentos_df <- df %>%
     filter(str_detect(tolower(texto_tramitacao), regex(apr_requerimentos_regex, ignore_case = TRUE))) %>%
-    mutate(evento = stringr::str_extract_all(tolower(texto_tramitacao), regex(apr_requerimentos_regex, ignore_case = TRUE))) %>% 
+    mutate(
+      evento = str_extract_all(tolower(texto_tramitacao), regex(apr_requerimentos_regex, ignore_case = TRUE))) %>%
     unnest() %>%
-    unique()
+    distinct()
+  apr_requerimentos_df <- apr_requerimentos_df %>%
+    mutate(
+      if(evento %in% colnames()) { 
+        numero_requerimento = gsub("^.*((aprova(do(s|)|)|deferido(s|).*) o(s|) requerimento(s|) nº(s|)\\s*)|\\s*(,).*$", "", evento)}
+    )
 }
 
 #' @title Cria coluna com as fases da tramitação no Senado
