659267b6b70c648d8c3b9ca8aa75dc444d23b367
Tarciso Oliveira <tarcisocomp@gmail.com>
2018-09-12 19:02:46 -0300

    Parametriza script para receber ID do PL, facilitando testes com diferentes PLs.

--- scripts/calcula-energia-historico.R
+++ scripts/calcula-energia-historico.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 
-proc_tram_camara <- readr::read_csv("/local/tarciso/workspace/agora-digital/data/camara/2056568-fases-tramitacao-camara.csv")
+pl_id <- 2121442
+proc_tram_camara <- readr::read_csv(paste0("/local/tarciso/workspace/agora-digital/data/camara/",pl_id,"-fases-tramitacao-camara.csv"))
 
 start_date = dplyr::last(proc_tram_camara$data_hora) - lubridate::days(90)
 hist_energia <- get_historico_energia_recente(proc_tram_camara) %>%
@@ -19,38 +20,3 @@ ggplot(hist_energia, aes(x=semana, y=energia_recente)) +
   # ggplot(hist_energia, aes(x=semana, y=energia_recente)) +
 #   geom_point() +
 #   geom_line()
-
-
-
-p
-
-x <- rnorm(100, 0, 3)
-
-logsumexp <- function (x) {
-  y = max(x)
-  y + log(sum(exp(x - y)))
-}
-
-softmax <- function (x) {
-  exp(x - logsumexp(x))
-}
-
-inv_logit <- function(x) {
-  return(1 / (1 + exp(- x)))
-}
-
-sigmoid = function(x) {
-  1 / (1 + exp(-x))
-}
-
-sigmoid1 <- function(x) {
-  return(x/(1+abs(x)))
-}
-
-sigmoid2 <- function(x) {
-  return(x/(sqrt(1+x^2)))
-}
-
-y <- test(x)
-
-plot(y ~ x)
