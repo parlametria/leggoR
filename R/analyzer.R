5b9cd4586f0a21a4be22ae3be607605b2ecad1d2
Tarciso Oliveira <tarcisocomp@gmail.com>
2018-12-19 15:01:37 -0300

    Aplica função rowwise ao dataframe antes de corrigir os nomes dos locais para corrigir o erro que estava acontecendo no script fetch_agenda.R

--- R/analyzer.R
+++ R/analyzer.R
@@ -223,10 +223,12 @@ extract_pauta <- function(agenda, tabela_geral_ids_casa, export_path) {
 #' fix_nomes_locais(pauta_df)
 fix_nomes_locais <- function(pautas_df) {
   pautas_locais_clean <- pautas_df %>%
+    dplyr::rowwise() %>%
     dplyr::mutate(local_clean = stringr::str_split(local, ' - ')[[1]][1]) %>%
     dplyr::mutate(local_clean = dplyr::if_else(local_clean == 'Plenário da Câmara dos Deputados' || local_clean == 'PLEN', 'Plenário', local_clean)) %>%
     dplyr::mutate(local_clean = dplyr::if_else(grepl("\\d",local_clean),'Comissão Especial', local_clean)) %>%
     dplyr::mutate(local = local_clean) %>%
+    dplyr::ungroup() %>%
     dplyr::select(-local_clean)
   
   return(pautas_locais_clean)
