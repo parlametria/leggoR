cb890949f31b22a2a9ddca5806d7aa6954d411b2
Juan Barros <juanlyrabarros@gmail.com>
2019-03-01 15:57:07 -0300

    Adiciona teste para extract_evento_Senado()

---
+++ tests/testthat/test_senado_analyzer.R
@@ -0,0 +1,33 @@
+context("senado_analyzer")
+
+# Setup
+setup <- function(){
+  SENADO_ID <<- c(91341, 127753, 129808, 133943, 96813, 120768)
+  DATA_INICIO <<- '2018-07-03'
+  DATA_FIM <<- '2018-07-10'
+  senado_df <<- as.data.frame(SENADO_ID)
+  eventos <- as.data.frame(SENADO_ID) %>%
+      dplyr::rowwise() %>%
+      dplyr::do(extract_evento_Senado(fetch_tramitacao(.$SENADO_ID, 'senado')))
+  return(TRUE)
+}
+
+check_api <- function(){
+  tryCatch(setup(), error = function(e){return(FALSE)})
+}
+
+test <- function(){
+  
+  test_that("extract_evento_Senado() returns dataframe", {
+    expect_true(is.data.frame(eventos))
+  })
+  
+  test_that('extract_evento_Senado() colnames', {
+    expect_true(all(names(eventos) %in% .COLNAMES_EVENTO_SEN))
+  })
+
+}
+
+if(check_api()){
+  test()
+} else testthat::skip('Erro no setup!')
