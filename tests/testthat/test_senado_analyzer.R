819e94bdfecc6330cff92ecab8ccd0f02ab0a5a0
Juan Barros <juanlyrabarros@gmail.com>
2019-03-01 16:30:12 -0300

    Adiciona teste para extract_evento_Senado()

--- tests/testthat/test_senado_analyzer.R
+++ tests/testthat/test_senado_analyzer.R
@@ -3,10 +3,8 @@ context("senado_analyzer")
 # Setup
 setup <- function(){
   SENADO_ID <<- c(91341, 127753, 129808, 133943, 96813, 120768)
-  DATA_INICIO <<- '2018-07-03'
-  DATA_FIM <<- '2018-07-10'
   senado_df <<- as.data.frame(SENADO_ID)
-  eventos <- as.data.frame(SENADO_ID) %>%
+  SENADO_EVENTOS <- as.data.frame(SENADO_ID) %>%
       dplyr::rowwise() %>%
       dplyr::do(extract_evento_Senado(fetch_tramitacao(.$SENADO_ID, 'senado')))
   return(TRUE)
@@ -19,11 +17,11 @@ check_api <- function(){
 test <- function(){
   
   test_that("extract_evento_Senado() returns dataframe", {
-    expect_true(is.data.frame(.$eventos))
+    expect_true(is.data.frame(SENADO_EVENTOS))
   })
   
   test_that('extract_evento_Senado() colnames', {
-    expect_true(all(names(.$eventos) %in% .COLNAMES_EVENTO_SEN))
+    expect_true(all(names(SENADO_EVENTOS) %in% .COLNAMES_EVENTO_SEN))
   })
 
 }
