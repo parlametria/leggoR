0a953d2839ee1fedddb512a7bc82348d0a98e2d1
Jair Guedes <jairguedesferreira@gmail.com>
2019-04-26 09:36:30 -0300

    Adiciona mais testes

--- tests/testthat/test_progresso.R
+++ tests/testthat/test_progresso.R
@@ -21,10 +21,13 @@ test <- function() {
   test_that('progresso mpv', {
     agenda <- tibble::as_tibble()
     pautas <- tibble::tribble(~data, ~sigla, ~id_ext, ~local, ~casa, ~semana, ~ano)
+    id_mpv_em_tramitacao <- 135061
+    id_mpv_tramitacao_encerrada <- 134134
+    
     etapas <- list()
-    etapas %<>% append(list(process_etapa(134134, "senado", agenda, pautas = pautas)))
+    etapas %<>% append(list(process_etapa(id_mpv_tramitacao_encerrada, "senado", agenda, pautas = pautas)))
     etapas %<>% purrr::pmap(dplyr::bind_rows)
-    progresso <-
+    progresso_mpv_tramitacao_encerrada <-
       agoradigital::generate_progresso_df_mpv(etapas$fases_eventos) %>% 
       dplyr::mutate(data_inicio = as.character(data_inicio),
                     data_fim = as.character(data_fim))
@@ -34,12 +37,33 @@ test <- function() {
                       'senado',134134,'Comissão Mista','2018-08-17 12:00:00','2018-09-17 12:00:00',
                       'senado',134134,'Câmara dos Deputados','2018-09-17 12:00:00','2018-11-13 12:00:00',
                       'senado',134134,'Senado Federal','2018-11-13 12:00:00','2018-11-21 12:00:00',
-                      'senado',134134,'Câmara dos Deputados','2018-11-21 12:00:00','2018-12-27 12:00:00',
+                      'senado',134134,'Câmara dos Deputados - Revisão','2018-11-21 12:00:00','2018-12-27 12:00:00',
                       'senado',134134,'Transformada em Lei','2018-12-27 12:00:00','2018-12-27 12:00:00',
                       'senado',134134,'Transformada em Lei com vetos',NA,NA) %>% 
       dplyr::mutate(prop_id = as.integer(prop_id))
     
-    expect_true(nrow(dplyr::anti_join(progresso, progresso_134134)) == 0)
+    expect_true(nrow(dplyr::anti_join(progresso_mpv_tramitacao_encerrada, progresso_134134)) == 0)
+    
+    etapas <- list()
+    etapas %<>% append(list(process_etapa(id_mpv_em_tramitacao, "senado", agenda, pautas = pautas)))
+    etapas %<>% purrr::pmap(dplyr::bind_rows)
+    progresso_mpv_em_tramitacao <-
+      agoradigital::generate_progresso_df_mpv(etapas$fases_eventos) %>% 
+      dplyr::mutate(data_inicio = as.character(data_inicio),
+                    data_fim = as.character(data_fim))
+    
+    progresso_135061 <- 
+      tibble::tribble(~ casa, ~ prop_id, ~ fase_global, ~ data_inicio, ~ data_fim,
+                      'senado',135061,'Comissão Mista','2018-12-28 12:00:00',NA,
+                      'senado',135061,'Câmara dos Deputados',NA,NA,
+                      'senado',135061,'Senado Federal',NA,NA,
+                      'senado',135061,'Câmara dos Deputados - Revisão',NA,NA,
+                      'senado',135061,'Transformada em Lei',NA,NA,
+                      'senado',135061,'Transformada em Lei com vetos',NA,NA) %>% 
+      dplyr::mutate(prop_id = as.integer(prop_id))
+    
+    expect_true(nrow(dplyr::anti_join(progresso_mpv_em_tramitacao, progresso_135061)) == 0)
+    
     
   })
 }
