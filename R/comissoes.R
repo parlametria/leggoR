c50deb9fc743bd7645db0b251c52526802731d67
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-05-14 14:17:32 -0300

    Conserta URL que retorna lista de comissões para o Senado. Adiciona função que baixa a lista de comissões do Congresso Nacional (permanentes, MPVs, mistas/especiais, e de análise dos vetos). Modifica função fetch_all_composicao_comissao() para baixar de todas as casas.

--- R/comissoes.R
+++ R/comissoes.R
@@ -187,24 +187,28 @@ fetch_composicao_comissoes_senado <- function(sigla) {
 #' @importFrom dplyr %>%
 #' @export
 fetch_all_composicao_comissao <- function() {
-  siglas_comissoes <- 
-    fetch_orgaos_camara() %>% 
+  orgaos_camara <- fetch_orgaos_camara()
+  
+  siglas_camara <- 
+    orgaos_camara %>% 
     dplyr::filter(tipo_orgao_id %in% c(2)) %>%
     dplyr::mutate_all(as.character) %>%
     dplyr::select(sigla) %>%
     dplyr::mutate(casa = 'camara',
                   sigla = trimws(sigla)) %>%
     dplyr::filter(sigla != 'PLEN')
+  
+  siglas_senado <- fetch_orgaos_senado() %>% 
+    dplyr::mutate(casa = 'senado', 
+                  sigla = stringr::str_replace_all(sigla, " ", ""))
+  
+  siglas_cong_nacional <- fetch_orgaos_congresso_nacional() %>%
+    dplyr::mutate(casa = 'congresso_nacional', 
+                  sigla = stringr::str_replace_all(sigla, " ", ""))
 
-  siglas_comissoes <-
-    rbind(siglas_comissoes,
-          fetch_orgaos_senado() %>%
-            dplyr::mutate(casa = 'senado',
-                          sigla = stringr::str_replace_all(sigla, " ", ""))) %>%
+  siglas_comissoes <- rbind(siglas_camara, siglas_senado, siglas_cong_nacional) %>%
     dplyr::distinct()
   
-  orgaos_camara <- fetch_orgaos_camara()
-
   composicao_comissoes <-
     purrr::map2_df(siglas_comissoes$sigla, siglas_comissoes$casa, ~ fetch_composicao_comissao(.x, .y, orgaos_camara)) %>%
     dplyr::mutate(partido = trimws(partido))
--- R/orgaos.R
+++ R/orgaos.R
@@ -21,36 +21,121 @@ fetch_orgaos_camara <- function() {
 #' @importFrom RCurl getURL
 #' @importFrom dplyr %>%
 fetch_orgaos_senado <- function() {
-  url_base <- 'https://www.congressonacional.leg.br/dados/comissao/lista/'
+  url_base <- 'http://legis.senado.leg.br/dadosabertos/comissao/lista/'
+  
+  comissoes_permanentes_df <- tibble::tibble()
+  comissoes_temporarias_df <- tibble::tibble()
+  cpis_df <- tibble::tibble()
 
   url_comissoes_permanentes <- RCurl::getURL(paste0(url_base, 'permanente'))
   
-  url_comissoes_especiais <- RCurl::getURL(paste0(url_base, 'mistaEspecial'))
+  url_comissoes_temporarias <- RCurl::getURL(paste0(url_base, 'temporaria'))
 
-  url_comissoes_mistas <- RCurl::getURL(paste0(url_base, 'mistas'))
+  url_cpis <- RCurl::getURL(paste0(url_base, 'cpi'))
+  
+  comissoes_permanentes_df <-
+    XML::xmlToDataFrame(nodes = XML::getNodeSet(
+      XML::xmlParse(url_comissoes_permanentes),
+      "//colegiado")) %>%
+    dplyr::select(sigla = SiglaColegiado) %>%
+    dplyr::filter(!is.na(sigla))
+
+  comissoes_temporarias_tmp <-
+    XML::xmlToDataFrame(nodes = XML::getNodeSet(
+      XML::xmlParse(url_comissoes_temporarias),
+      "//colegiado"))
+  
+  if (ncol(comissoes_temporarias_tmp) > 1) {
+    comissoes_temporarias_df <- comissoes_temporarias_tmp %>% 
+      dplyr::select(sigla = SiglaColegiado) %>%
+      dplyr::filter(!is.na(sigla))
+  }
+    
+  cpis_tmp <-
+    XML::xmlToDataFrame(nodes = XML::getNodeSet(
+      XML::xmlParse(url_cpis),
+      "//colegiado"))
+  
+  if (ncol(cpis_tmp) > 1) {
+    cpis_df <- cpis_tmp %>% 
+      dplyr::select(sigla = SiglaColegiado) %>%
+      dplyr::filter(!is.na(sigla))
+  }
+  
+  df <-
+    rbind(comissoes_permanentes_df, comissoes_temporarias_df, cpis_df) %>%
+    dplyr::distinct()
+
+  return(df)
+}
+
+#' @title Baixa todas as siglas das comissões atuais do Congresso Nacional
+#' @description Retorna um dataframe contendo as siglas das comissões atuais do Congresso Nacional
+#' @return Dataframe
+#' @examples
+#' fetch_orgaos_cn()
+#' @importFrom RCurl getURL
+#' @importFrom dplyr %>%
+fetch_orgaos_congresso_nacional <- function() {
+  url_base <- 'https://www.congressonacional.leg.br/dados/comissao/lista/'
+  
+  comissoes_permanentes_df <- tibble::tibble()
+  comissoes_mpvs_df <- tibble::tibble()
+  comissoes_mistas_especiais_df <- tibble::tibble()
+  comissoes_analise_veto_df <- tibble::tibble()
+  
+  url_comissoes_permanentes <- RCurl::getURL(paste0(url_base, 'permanente'))
+  
+  url_comissoes_mpvs <- RCurl::getURL(paste0(url_base, 'mpv'))
+  
+  url_comissoes_mistas_especiais <- RCurl::getURL(paste0(url_base, 'mistaEspecial'))
+  
+  url_comissoes_analise_veto <- RCurl::getURL(paste0(url_base, 'veto'))
   
   comissoes_permanentes_df <-
     XML::xmlToDataFrame(nodes = XML::getNodeSet(
       XML::xmlParse(url_comissoes_permanentes),
       "//Colegiado")) %>%
-    dplyr::select(sigla = SiglaColegiado)
-
-  #comissoes_temporarias_df <-
-  #  XML::xmlToDataFrame(nodes = XML::getNodeSet(
-  #    XML::xmlParse(url_comissoes_temporarias),
-  #    "//Colegiado")) %>%
-  #  dplyr::select(sigla = SiglaColegiado)
+    dplyr::select(sigla = SiglaColegiado) %>%
+    dplyr::filter(!is.na(sigla))
   
-  comissoes_mistas_df <-
+  comissoes_mpvs_tmp <-
     XML::xmlToDataFrame(nodes = XML::getNodeSet(
-      XML::xmlParse(url_comissoes_mistas),
-      "//IdentificacaoComissao")) %>%
-    dplyr::select(sigla = SiglaComissao)
-
+      XML::xmlParse(url_comissoes_mpvs),
+      "//Colegiado"))
+  
+  if (ncol(comissoes_mpvs_tmp) > 1) {
+    comissoes_mpvs_df <- comissoes_mpvs_tmp %>% 
+      dplyr::select(sigla = SiglaColegiado) %>%
+      dplyr::filter(!is.na(sigla))
+  }
+  
+  comissoes_mistas_especiais_tmp <-
+    XML::xmlToDataFrame(nodes = XML::getNodeSet(
+      XML::xmlParse(url_comissoes_mistas_especiais),
+      "//Colegiado"))
+  
+  if (ncol(comissoes_mistas_especiais_tmp) > 1) {
+    comissoes_mistas_especiais_df <- comissoes_mistas_especiais_tmp %>% 
+      dplyr::select(sigla = SiglaColegiado) %>%
+      dplyr::filter(!is.na(sigla))
+  }
+  
+  comissoes_analise_veto_tmp <-
+    XML::xmlToDataFrame(nodes = XML::getNodeSet(
+      XML::xmlParse(url_comissoes_analise_veto),
+      "//Colegiado"))
+  
+  if (ncol(comissoes_analise_veto_tmp) > 1) {
+    comissoes_analise_veto_df <- comissoes_analise_veto_tmp %>% 
+      dplyr::select(sigla = SiglaColegiado) %>%
+      dplyr::filter(!is.na(sigla))
+  }
+  
   df <-
-    rbind(comissoes_permanentes_df, comissoes_mistas_df) %>%
-    rbind(comissoes_mistas_df) %>%
+    rbind(comissoes_permanentes_df, comissoes_mpvs_df, 
+          comissoes_mistas_especiais_df, comissoes_analise_veto_df) %>%
     dplyr::distinct()
-
+  
   return(df)
 }
