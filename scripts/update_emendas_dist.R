6d0095992e9459a8dd98c27f090cfeffd54fd4f2
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-06-11 18:53:28 -0300

    Remove instalação do pacote do script. Ficará no script que chama esse.

--- scripts/update_emendas_dist.R
+++ scripts/update_emendas_dist.R
@@ -18,9 +18,6 @@ distances_folderpath <- args[2]
 emendas_raw_filepath <- args[3]
 processed_emendas_filepath <- args[4]
 
-## Install local repository R package version
-devtools::install()
-
 ## Read emendas csv, add their distances and export the new emendas csv file
 files <- list.files(path=unformatted_distances_folderpath, pattern="*.csv", full.names=TRUE, recursive=FALSE)
 emendas_distances_list <- purrr::map(files, ~ agoradigital::format_table_distances_to_emendas(distancias_datapath = .x, write_datapath=distances_folderpath))
