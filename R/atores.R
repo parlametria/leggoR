8d3746970b83eb7cec637297304053846f0e68e2
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2019-06-19 16:05:43 -0300

    Adicionando informaçes sobre o tipo do documento

--- R/atores.R
+++ R/atores.R
@@ -2,16 +2,19 @@ atores <- function(documentos_df, autores_df) {
   atores <- merge(documentos_df, autores_df, by = "id_documento") %>%
     dplyr::select(casa = casa.x,
                   id_autor,
-                  nome,
+                  nome_autor = nome,
                   sigla_tipo,
                   codTipo,
                   id_documento)
 
   atores <- atores %>%
     dplyr::group_by(sigla_tipo,
-                    nome,
+                    nome_autor,
                     id_autor,
-                    casa,
-                    id_documento) %>%
-    dplyr::summarise(qtd = dplyr::n())
+                    codTipo,
+                    casa) %>%
+    dplyr::summarise(qtd_de_documentos = dplyr::n())
+
+  tipos_props <- rcongresso::fetch_tipos_proposicao()
+  atores <- merge(atores, tipos_props, by.x = "codTipo", by.y = "cod")
 }
