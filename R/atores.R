71dca76c43b0571a46f76202380f80d7d8a3938e
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-06-25 10:00:15 -0300

    reordena qtd de documentos e adiciona coluna de prop principal

--- R/atores.R
+++ R/atores.R
@@ -4,26 +4,25 @@
 #' @return Dataframe
 #' @export
 create_tabela_atores <- function(documentos_df, autores_df) {
-  atores <- merge(documentos_df, autores_df, by = "id_documento") %>%
-    dplyr::select(casa = casa.x,
+  atores <- merge(documentos_df, autores_df, by = c("id_documento", "casa")) %>%
+    dplyr::select(casa,
                   id_autor,
                   nome_autor = nome,
                   sigla_tipo,
                   codTipo,
                   id_documento,
+                  id_principal,
                   descricao_tipo = descricaoTipo)
 
   atores <- atores %>%
-    dplyr::group_by(sigla_tipo,
-                    nome_autor,
-                    id_autor,
-                    codTipo,
+    dplyr::group_by(id_principal,
                     casa,
+                    id_autor,
+                    nome_autor,
+                    codTipo,
+                    sigla_tipo,
                     descricao_tipo) %>%
-    dplyr::summarise(qtd_de_documentos = dplyr::n())
+    dplyr::summarise(qtd_de_documentos = dplyr::n()) %>%
+    dplyr::arrange(id_principal, -qtd_de_documentos)
 
-  tipos_props <- rcongresso::fetch_tipos_proposicao()
-  atores <- merge(atores, tipos_props, by.x = "codTipo", by.y = "cod") %>%
-    dplyr::select(-nome,
-                  -sigla)
 }
