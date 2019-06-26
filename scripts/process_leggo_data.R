949dcfb806b00607652e4803afdffea611e11c4c
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-06-26 09:18:53 -0300

    Adiciona script que gera tabela de atores.

---
+++ scripts/process_leggo_data.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+library(magrittr)
+
+help <- "
+Usage:
+Rscript process_leggo_data.R <input_path> <output_path>
+"
+
+#Functions
+
+## Process args
+args <- commandArgs(trailingOnly = TRUE)
+min_num_args <- 2
+if (length(args) < min_num_args) {
+    stop(paste("Wrong number of arguments!", help, sep = "\n"))
+}
+input_path <- args[1]
+output_path <- args[2]
+
+## Install local repository R package version
+devtools::install()
+library(magrittr)
+
+# Read current data csvs
+
+## Read PLs list
+documentos <- readr::read_csv(paste0(input_path,'/documentos.csv'),
+                                        col_types = list(
+                                            .default = readr::col_character(),
+                                            id_documento = readr::col_double(),
+                                            id_principal = readr::col_double(),
+                                            numero = readr::col_integer(),
+                                            ano = readr::col_integer(),
+                                            data_apresentacao = readr::col_datetime(format = ""),
+                                            codTipo = readr::col_integer(),
+                                            statusProposicao.codSituacao = readr::col_integer(),
+                                            statusProposicao.codTipoTramitacao = readr::col_integer(),
+                                            statusProposicao.dataHora = readr::col_datetime(format = ""),
+                                            statusProposicao.sequencia = readr::col_integer()
+                                        ))
+
+autores <- readr::read_csv(paste0(input_path, '/autores.csv'),
+                                   col_types = list(
+                                     .default = readr::col_character(),
+                                     id_autor = readr::col_character(),
+                                     nome = readr::col_character(),
+                                     cod_tipo = readr::col_integer(),
+                                     tipo = readr::col_character(),
+                                     uri = readr::col_character(),
+                                     id_documento = readr::col_character(),
+                                     casa = readr::col_character()
+                                   ))
+
+print(paste("Gerando tabela de atores a partir de dados atualizados de documentos e autores..."))
+
+atores_df <- agoradigital::create_tabela_atores(documentos, autores)
+
+readr::write_csv(atores_df, paste0(output_path, '/atores.csv'))
