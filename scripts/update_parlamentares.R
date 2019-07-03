6c0e7abd04f3695ce7c160dc4992d9952372c0cd
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2019-07-03 10:47:19 -0300

    Update que atualiza parlamentares pronto para deputados

--- scripts/update_parlamentares.R
+++ scripts/update_parlamentares.R
@@ -18,14 +18,6 @@ export_path <- args[1]
 devtools::install()
 
 # Read current data csvs
-current_deputados <- readr::read_csv(paste0(export_path,'/deputados.csv'),
-                                          col_types = list(
-                                          .default = readr::col_character(),
-                                          data_falecimento = readr::col_date(format = ""),
-                                          data_nascimento = readr::col_date(format = ""),
-                                          id = readr::col_double(),
-                                          ultimo_status_gabinete_andar = readr::col_double(),
-                                          ultimo_status_gabinete_sala = readr::col_double(),
-                                          ultimo_status_id = readr::col_double(),
-                                          ultimo_status_id_legislatura = readr::col_double()
-                                    ))
+deputados <- rcongresso::fetch_all_deputados()
+
+readr::write_csv(deputados, paste0(export_path, "/deputados.csv"))
