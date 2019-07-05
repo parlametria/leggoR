78acc7765166adb8dbea76fa9fd52a2395c8bb06
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-07-05 15:02:05 -0300

    Remove versão da biblioteca stringr do DESCRIPTION. Remove uso de função da biblioteca stringr que não está disponível na versão instalada no docker. Corrige código que causava erro.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM rocker/tidyverse:3.4.4
 
 RUN mkdir /agora-digital
 WORKDIR /agora-digital
-COPY DESCRIPTION .
+COPY . .
 RUN Rscript -e 'devtools::install_deps()'
 RUN Rscript -e 'devtools::install_github("analytics-ufcg/rcongresso")'
-COPY . .
 RUN Rscript -e 'devtools::install()'
--- scripts/fetch_updated_bills_data.R
+++ scripts/fetch_updated_bills_data.R
@@ -19,9 +19,7 @@ export_path <- args[2]
 devtools::install()
 
 ## Read PLs list and export their data
-print(packageVersion("stringr"))
-stringr::str_starts(c("apple", "banana", "pear", "pineapple"), "p")
 readr::read_csv(pls_ids_filepath) %>%
-	dplyr::mutate(row_num = 1:dplyr::n()) %>%
+	dplyr::mutate(row_num = 1:nrow(.)) %>%
 	dplyr::select(row_num,id_camara,id_senado,apelido,tema) %>%
 	agoradigital::export_data(export_path)
