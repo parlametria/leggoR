21c16e3a2ab3d12c5060fe71765701f38e0e3978
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-07-10 09:45:28 -0300

    adapta script para autores e documentos do senado

--- scripts/update_leggo_data.R
+++ scripts/update_leggo_data.R
@@ -17,17 +17,15 @@ get_fetch_status <- function(docs_ids, docs_data, authors_data) {
   }
 
   fetched_data_docs <- docs_data %>%
-    dplyr::select(id_documento, casa) %>%
-    dplyr::mutate(id_documento = as.numeric(id_documento))
+    dplyr::select(id_documento, casa)
 
   fetched_autor_docs <- authors_data %>%
-    dplyr::select(id_documento, casa) %>%
-    dplyr::mutate(id_documento = as.numeric(id_documento))
+    dplyr::select(id_documento, casa)
 
   complete_docs_df <- dplyr::inner_join(docs_ids,
-                                     dplyr::inner_join(fetched_data_docs,fetched_autor_docs,
-                                                       by=c("id_documento","casa")),
-                                     by=c("id_documento","casa"))
+                                        dplyr::inner_join(fetched_data_docs,fetched_autor_docs,
+                                                          by=c("id_documento","casa")),
+                                        by=c("id_documento","casa"))
 
   incomplete_docs_df <- dplyr::anti_join(docs_ids, complete_docs_df,
                                          by=c("id_documento","id_principal","casa"))
@@ -39,7 +37,7 @@ get_fetch_status <- function(docs_ids, docs_data, authors_data) {
 args <- commandArgs(trailingOnly = TRUE)
 min_num_args <- 2
 if (length(args) < min_num_args) {
-    stop(paste("Wrong number of arguments!", help, sep = "\n"))
+  stop(paste("Wrong number of arguments!", help, sep = "\n"))
 }
 pls_ids_filepath <- args[1]
 export_path <- args[2]
@@ -59,30 +57,43 @@ pls_ids <- readr::read_csv(pls_ids_filepath,
                            ))
 
 current_docs <- readr::read_csv(paste0(export_path,'/documentos.csv'),
-                                        col_types = list(
-                                            .default = readr::col_character(),
-                                            id_documento = readr::col_double(),
-                                            id_principal = readr::col_double(),
-                                            numero = readr::col_integer(),
-                                            ano = readr::col_integer(),
-                                            data_apresentacao = readr::col_datetime(format = ""),
-                                            codTipo = readr::col_integer(),
-                                            statusProposicao.codSituacao = readr::col_integer(),
-                                            statusProposicao.codTipoTramitacao = readr::col_integer(),
-                                            statusProposicao.dataHora = readr::col_datetime(format = ""),
-                                            statusProposicao.sequencia = readr::col_integer()
-                                        ))
+                                col_types = list(
+                                  .default = readr::col_character(),
+                                  id_documento = readr::col_double(),
+                                  id_principal = readr::col_double(),
+                                  numero = readr::col_integer(),
+                                  ano = readr::col_integer(),
+                                  data_apresentacao = readr::col_datetime(format = ""),
+                                  codTipo = readr::col_integer(),
+                                  statusProposicao.codSituacao = readr::col_integer(),
+                                  statusProposicao.codTipoTramitacao = readr::col_integer(),
+                                  statusProposicao.dataHora = readr::col_datetime(format = ""),
+                                  statusProposicao.sequencia = readr::col_integer(),
+                                  casa = readr::col_character(),
+                                  sigla_tipo = readr::col_character()
+                                ))
 
 current_autores <- readr::read_csv(paste0(export_path, '/autores.csv'),
                                    col_types = list(
                                      .default = readr::col_character(),
                                      id_autor = readr::col_character(),
-                                     nome = readr::col_character(),
+                                     nome_autor = readr::col_character(),
+                                     nome_completo = readr::col_character(),
+                                     sexo = readr::col_character(),
+                                     sigla_partido = readr::col_character(),
+                                     sigla_tipo_autor = readr::col_character(),
+                                     uf_autor = readr::col_character(),
+                                     uf_parlamentar = readr::col_character(),
+                                     url_foto = readr::col_character(),
+                                     url_pagina = readr::col_character(),
                                      cod_tipo = readr::col_integer(),
                                      tipo = readr::col_character(),
                                      uri = readr::col_character(),
                                      id_documento = readr::col_character(),
-                                     casa = readr::col_character()
+                                     casa = readr::col_character(),
+                                     email = readr::col_character(),
+                                     indicador_outros_autores = readr::col_character(),
+                                     num_ordem_autor = readr::col_character()
                                    ))
 
 # Check for new data
