c425220af54c13d1a42647fdbdb3aa7e2fd11d69
Jair Guedes <jairguedesferreira@gmail.com>
2019-08-14 16:38:24 -0300

    Ajeita testes

--- tests/testthat/test-export-data.R
+++ tests/testthat/test-export-data.R
@@ -1,24 +1,24 @@
 context('test-export-data.R')
 
 pautas <- tibble::tribble(~data, ~sigla, ~id_ext, ~local, ~casa, ~semana, ~ano)
-process_etapa_data <- process_etapa(91341, 'senado', tibble::tibble(), pautas)
+process_etapa_data <- process_etapa(91341, 'senado', pautas)
 proposicao <- process_etapa_data$proposicao
 tram <<- process_etapa_data$fases_eventos
 temperatura <- process_etapa_data$hist_temperatura
-emendas <- process_etapa_data$emendas
+#emendas <- process_etapa_data$emendas
 
 test_that('get_ementas_in_camara() is dataframe', {
   expect_true(is.data.frame(proposicao))
   expect_true(is.data.frame(tram))
   expect_true(is.data.frame(temperatura))
-  expect_true(is.data.frame(emendas))
+#  expect_true(is.data.frame(emendas))
 })
 
 test_that("get_ementas_in_camara() don't return empty", {
   expect_true(nrow(proposicao) != 0)
   expect_true(nrow(tram) != 0)
   expect_true(nrow(temperatura) != 0)
-  expect_true(nrow(emendas) != 0)
+  #expect_true(nrow(emendas) != 0)
 })
 
 test_that('process_etapa() is list', {
