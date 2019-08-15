b22c38796940fdc7615a8d7b6166555c7a65af55
jairNeto <jairguedesferreira@gmail.com>
2019-08-15 16:44:02 -0300

    Update test-export-data.R

--- tests/testthat/test-export-data.R
+++ tests/testthat/test-export-data.R
@@ -5,13 +5,11 @@ process_etapa_data <- process_etapa(91341, 'senado', pautas)
 proposicao <- process_etapa_data$proposicao
 tram <<- process_etapa_data$fases_eventos
 temperatura <- process_etapa_data$hist_temperatura
-#emendas <- process_etapa_data$emendas
 
 test_that('get_ementas_in_camara() is dataframe', {
   expect_true(is.data.frame(proposicao))
   expect_true(is.data.frame(tram))
   expect_true(is.data.frame(temperatura))
-#  expect_true(is.data.frame(emendas))
 })
 
 test_that("get_ementas_in_camara() don't return empty", {
