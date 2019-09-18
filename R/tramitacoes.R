ec87f68becb5c4c89f6f480fbb8fc4183b59b423
Jair Guedes <jairguedesferreira@gmail.com>
2019-09-18 15:29:36 -0300

    Modificações no fetch_tramitação

--- R/tramitacoes.R
+++ R/tramitacoes.R
@@ -2,10 +2,11 @@
 #' @description Retorna dataframe com os dados da movimentação da proposição, incluindo tramitação, prazos, despachos e situação
 #' Ao fim, a função retira todos as colunas que tenham tipo lista para uniformizar o dataframe.
 #' @param prop_id ID de uma proposição do Senado
+#' @param data_ref Data de referencia a partir da qual a tramitação será retornada
 #' @return Dataframe normalizado com as informações sobre a movimentação de uma proposição no Senado
 #' @examples
 #' fetch_tramitacao_senado(91341)
-fetch_tramitacao_senado <- function(prop_id, data_ref = NA, isMPV = FALSE) {
+fetch_tramitacao_senado <- function(prop_id, data_ref = NA) {
     tramitacao <- 
       rcongresso::fetch_tramitacao_senado(prop_id, data_ref) %>%
       dplyr::mutate(data_hora = lubridate::ymd_hm(paste(data_hora, "00:00")),
@@ -25,12 +26,6 @@ fetch_tramitacao_senado <- function(prop_id, data_ref = NA, isMPV = FALSE) {
                     link_inteiro_teor,
                     origem_tramitacao_local_nome_casa_local,
                     destino_tramitacao_local_nome_casa_local)
-  
-  if(!isMPV) {
-    tramitacao <-
-      tramitacao %>% 
-      dplyr::select(-c(origem_tramitacao_local_nome_casa_local, destino_tramitacao_local_nome_casa_local))
-  }
     
     return (tramitacao)
 }
@@ -89,38 +84,51 @@ fetch_tramitacao_camara <- function(bill_id, data_inicio = NA, data_fim = NA) {
 #' @param export_path Path para ser escritos as tramitações
 #' @param id_documento Id do documento para ser baixado
 #' @param id_principal Id da proposição principal
-write_docs <- function(export_path, id_documento, id_principal) {
-  path <- paste0(export_path, "/", id_principal, "_camara")
+write_docs <- function(casa, export_path, id_documento, id_principal, dias_atras) {
+  path <- paste0(export_path, "/", id_principal, "_", casa)
   ifelse(!dir.exists(file.path(path)), dir.create(file.path(path)), FALSE)
-  destfile <- paste0(path, "/", id_documento, ".csv")
+  if (casa == 'camara') {
+    destfile <- paste0(path, "/", id_documento, ".csv")
+  }else {
+    destfile <- paste0(path, "/", id_principal, ".csv")
+  }
+  
   if(file.exists(destfile)){
-    tram_antigo <- 
+    tram_antigo <-
       readr::read_csv(destfile, col_types = readr::cols(
+        .default = readr::col_character(),
         prop_id = readr::col_double(),
-        casa = readr::col_character(),
         data_hora = readr::col_datetime(format = ""),
         sequencia = readr::col_double(),
-        texto_tramitacao = readr::col_character(),
-        sigla_local = readr::col_character(),
         id_situacao = readr::col_double(),
-        descricao_situacao = readr::col_character(),
-        link_inteiro_teor = readr::col_character(),
         id_principal = readr::col_double()
       ))
-    tram_novo <- 
-      fetch_tramitacao_camara(id_documento, lubridate::today() - 14, lubridate::today()) %>% 
-      dplyr::mutate(id_principal = id_documento)
+    
+    if (casa == 'camara') {
+      tram_novo <-
+        fetch_tramitacao_camara(id_documento, lubridate::today() - dias_atras, lubridate::today()) %>%
+        dplyr::mutate(id_principal = id_documento)
+    } else {
+      tram_novo <- 
+        fetch_tramitacao_senado(id_documento, format(lubridate::today() - dias_atras, "%Y%m%d")) %>% 
+        dplyr::mutate(id_principal = id_documento,
+                      link_inteiro_teor = dplyr::if_else(link_inteiro_teor == NA, '', link_inteiro_teor))
+    }
     tram <-
-      dplyr::bind_rows(tram_antigo, tram_novo) %>% 
-      unique()
-    readr::write_csv(tram, destfile, append = T)
+      dplyr::bind_rows(tram_antigo, tram_novo) %>%
+      unique() 
   } else {
-    tram <- 
-      fetch_tramitacao_camara(id_documento) %>% 
-      dplyr::mutate(id_principal = id_documento)
-    readr::write_csv(tram, destfile)
+    if (casa == 'camara') {
+      tram <- 
+        fetch_tramitacao_camara(id_documento) %>% 
+        dplyr::mutate(id_principal = id_documento)
+    } else {
+      tram <- 
+        fetch_tramitacao_senado(id_principal) %>% 
+        dplyr::mutate(id_principal = id_principal)
+    }
   }
-
+  readr::write_csv(tram, destfile)
 }
 
 #' @title Write_docs seguro
@@ -133,8 +141,12 @@ safe_write_docs <- purrr::safely(
 #' @param export_path Path para ser escritos as tramitações
 #' @param docs_ids Dataframe com os IDs dos documentos a serem baixados
 #' @export
-fetch_tramitacao_data <- function(export_path, docs_ids) {
-    purrr::pmap(list(export_path, docs_ids$id_documento, docs_ids$id_principal), function(a, b, c) safe_write_docs(a, b, c))
+fetch_tramitacao_data <- function(casa, export_path, docs_ids, dias_atras) {
+  if (casa == 'senado') {
+    purrr::pmap(list(casa, export_path, docs_ids$id_documento, docs_ids$id_principal, dias_atras), function(a, b, c, d, e) safe_write_docs(a, b, c, d, e))
+  }else {
+    purrr::pmap(list(casa, export_path, docs_ids$id_documento, docs_ids$id_principal, dias_atras), function(a, b, c, d, e) safe_write_docs(a, b, c, d, e))
+  }
 }
 
 #' @title Baixa os dados da tramitação de vários Projetos de Lei
