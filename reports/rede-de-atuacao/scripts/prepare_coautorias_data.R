28160f35a3be7b271c9e5ce28497a6f06847daf1
Jair Guedes <jairguedesferreira@gmail.com>
2019-10-15 16:52:03 -0300

    Melhora relatorio

--- reports/rede-de-atuacao/scripts/prepare_coautorias_data.R
+++ reports/rede-de-atuacao/scripts/prepare_coautorias_data.R
@@ -1,4 +1,38 @@
-generate_nodes_and_edges <- function(coautorias) {
+#' @title Calcula o tamanho dos nós
+#' @description Recebe um dataframe com as arestas e os nós, e calcula o tamanho do nó
+#' a partir do somatório dos pesos das arestas
+#' @param final_edges Dataframe com as arestas
+#' @param final_nodes Dataframe com os nós
+#' @param smoothing Variável para suavizar o tamanho dos nós
+#' @return Dataframe dos nós com a coluna node_size
+set_nodes_size <- function(final_edges, final_nodes, smoothing) {
+  nodes_size_src <- 
+    final_edges %>% 
+    group_by(node = source) %>% 
+    summarise(node_size = sum(value))
+  nodes_size_tgt <- 
+    final_edges %>% 
+    group_by(node = target) %>% 
+    summarise(node_size = sum(value))
+  nodes_size <-
+    bind_rows(nodes_size_src,nodes_size_tgt) %>%
+    group_by(node) %>% 
+    summarise(node_size = sum(node_size)) %>%
+    ungroup() %>% 
+    mutate(node = as.numeric(node))
+  final_nodes %>%
+    left_join(nodes_size, by=c("index"="node")) %>%
+    mutate(node_size = node_size / smoothing)
+}
+
+#' @title Gera os dataframe de nós e arestas
+#' @description Recebe um dataframe com coautorias e gera os dataframes com os nós e
+#' com as arestas 
+#' @param coautorias Dataframe com as coautorias
+#' @param edges_weight Variável para multiplicar com os pesos das arestas
+#' @param smoothing Variável para suavizar o tamanho dos nós
+#' @return Lista com os dataframes de arestas e dos nós
+generate_nodes_and_edges <- function(coautorias, edges_weight = 100, smoothing = 1) {
   library(tidygraph)
   
   graph_nodes <- dplyr::bind_rows(
@@ -41,42 +75,15 @@ generate_nodes_and_edges <- function(coautorias) {
     arrange(target) %>% 
     as.data.frame()
   
+  final_edges <- 
+    final_edges %>% 
+    mutate(value = value*edges_weight) 
+  
+  final_nodes <-
+    set_nodes_size(final_edges, final_nodes, smoothing) 
+  
   return(list(final_nodes,final_edges))
 }
-
-# generate_edges <- function(coautorias, nodes) {
-#   df <- 
-#     coautorias %>% 
-#     mutate(id_autor.x=as.character(id_autor.x),
-#            id_autor.y=as.character(id_autor.y))
-#   
-#   graph_edges <- coautorias %>%
-#     dplyr::select(
-#       source = id_autor.x,
-#       target = id_autor.y,
-#       value = peso_arestas) %>% 
-#     dplyr::mutate(source = as.factor(source),
-#                   target = as.factor(target)) %>% 
-#     inner_join(nodes, by = c("source" = "index")) %>% 
-#     inner_join(nodes, by = c("target" = "index")) %>% 
-#     select(source = index.x, target = index.y, value) %>%
-#     mutate(source = as.factor(source), target = as.factor(target)) %>% 
-#     arrange(target) %>% 
-#     as.data.frame()
-# }
-
-# generate_nodes_and_edges <- function(coautorias) {
-#   
-#   coautorias <- coautorias %>% 
-#     distinct()
-#   
-#   nodes <- generate_nodes(coautorias)
-#   
-#   edges <-
-#     generate_edges(coautorias %>% select(id_autor.x, id_autor.y, peso_arestas), nodes)
-#   
-#   return(list(nodes, edges))
-# }
 
 #' @title Concateca dois elementos com um separador no meio
 #' @description Recebe duas variáveis x e y e retorna a união "x:y".
@@ -84,7 +91,7 @@ generate_nodes_and_edges <- function(coautorias) {
 #' @param y Segunda variável a ser concatenada
 #' @param sep Separador a ser concatenado
 #' @return String concatenada com a primeira variável + separador + segunda variável
-paste_cols <- function(x, y, sep = ":") {
+paste_cols_sorted <- function(x, y, sep = ":") {
   stopifnot(length(x) == length(y))
   return(lapply(1:length(x), function(i) {
     paste0(sort(c(x[i], y[i])), collapse = sep)
@@ -92,22 +99,31 @@ paste_cols <- function(x, y, sep = ":") {
     unlist())
 }
 
+#' @title Remove arestas duplicadas
+#' @description Recebe um dataframe com autorias e remove as arestas
+#' duplicadas
+#' @param df Dataframe com as arestas duplicadas
+#' @return Dataframe sem as duplicadas
 remove_duplicated_edges <- function(df) {
   df %>%
     mutate(col_pairs =
-             paste_cols(id_autor.x,
-                        id_autor.y,
-                        sep = ":")) %>%
-    group_by(col_pairs) %>%
+             paste_cols_sorted(id_autor.x,
+                               id_autor.y,
+                               sep = ":")) %>%
+    distinct(id_principal, casa, id_documento, data = data.x, col_pairs) %>%
     tidyr::separate(col = col_pairs,
                     c("id_autor.x",
                       "id_autor.y"),
-                    sep = ":") %>%
-    group_by(id_autor.x, id_autor.y) %>%
-    distinct()
+                    sep = ":")
 }
 
-
+#' @title Cria o dataframe de coautorias
+#' @description  Recebe o dataframe de pesos, autorias e parlamentares e 
+#' cria o dataframe de coautorias
+#' @param peso_autorias Dataframe com as pesos das autorias
+#' @param autorias Dataframe com autorias
+#' @param parlamentares Dataframe com todos os dados dos parlamentars
+#' @return Dataframe
 get_coautorias <- function(peso_autorias, autorias, parlamentares) {
 
   peso_autorias <- 
@@ -123,7 +139,7 @@ get_coautorias <- function(peso_autorias, autorias, parlamentares) {
   coautorias <- coautorias %>%
     remove_duplicated_edges() %>%
     inner_join(peso_autorias, by = c("id_principal", "id_documento")) %>% 
-    group_by(id_principal, casa, id_autor.x, id_autor.y, data.x) %>% 
+    group_by(id_principal, casa, id_autor.x, id_autor.y, data) %>% 
     summarise(peso_arestas = sum(peso_arestas),
            num_coautorias = n()) %>%
     ungroup() %>% 
@@ -138,6 +154,11 @@ get_coautorias <- function(peso_autorias, autorias, parlamentares) {
   return(coautorias)
 }
 
+#' @title Cria o dataframe de autorias da camara
+#' @description  Faz um merge do df de documentos com autores
+#' @param docs_camara Dataframe com documentos da camara
+#' @param autores_camara Dataframe com autores da camara
+#' @return Dataframe
 prepare_autorias_df_camara <- function(docs_camara, autores_camara) {
   autores_docs <- merge(docs_camara, autores_camara, by = c("id_documento", "casa")) %>%
         dplyr::select(id_principal,
@@ -148,6 +169,11 @@ prepare_autorias_df_camara <- function(docs_camara, autores_camara) {
     dplyr::distinct()
 }
 
+#' @title Cria o dataframe de autorias da senado
+#' @description  Faz um merge do df de documentos com autores
+#' @param docs_senado Dataframe com documentos da senado
+#' @param autores_senado Dataframe com autores da senado
+#' @return Dataframe
 prepare_autorias_df_senado <- function(docs_senado, autores_senado) {
   autores_docs <- merge(docs_senado, autores_senado %>% dplyr::filter(!is.na(id_autor)), 
                         by = c("id_principal", "id_documento", "casa")) %>% 
@@ -159,6 +185,11 @@ prepare_autorias_df_senado <- function(docs_senado, autores_senado) {
     dplyr::distinct()
 }
 
+#' @title Cria o dataframe com os pesos das autorias
+#' @description  Calcula o peso de cada autoria, onde o peso é menor quanto
+#' maior o número de autores
+#' @param autorias Dataframe com as autorias
+#' @return Dataframe
 compute_peso_autoria_doc <- function(autorias) {
   peso_autorias <- autorias %>% 
     group_by(id_principal, id_documento) %>% 
