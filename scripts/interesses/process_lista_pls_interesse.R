84e19b93ef443b753763ae6a8c62cb755dc220be
gileadekelvin <gileadekelvin@gmail.com>
2020-03-19 14:15:49 -0300

    Adiciona checagem do valor da url

--- scripts/interesses/process_lista_pls_interesse.R
+++ scripts/interesses/process_lista_pls_interesse.R
@@ -3,11 +3,17 @@ library(here)
 
 #' @title Processa todas as PLs de diversos interesses e que são analisadas pelo LEGGO
 #' @description Junta todos as PL's listadas para os interesses analisados pelo LEGGO
+#' @param url URL para lista de interesses do leggo
 #' @return Dataframe com as PL's que serviram de entrada para o pipeline
 #' contendo id_camara,id_senado,apelido,tema,advocacy_link,keywords,tipo_agenda
 #' @example
 #' tabela_pls <- processa_lista_pls_interesses(url)
 processa_lista_pls_interesses <- function(url) {
+
+  if (is.null(url) | url == "") {
+    stop("URL para planilha de interesses precisa ser diferente de vazio e não nula.")
+  }
+
   interesses <- readr::read_csv(url)
 
   pls_para_analise <- purrr::pmap_dfr(list(interesses$interesse, interesses$url),
