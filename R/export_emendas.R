bc7b2e1b7f648d6476190b52b3e794d6b54fb5ed
Tarciso Braz <tarcisocomp@gmail.com>
2020-03-24 20:08:03 +0000

    Modifica que obtém dados de emendas para pegar a partir das matérias relacionadas.

--- R/export_emendas.R
+++ R/export_emendas.R
@@ -4,12 +4,18 @@
 #' @param casa senado ou camara
 #' @export
 get_emendas <- function(id, casa) {
-  cat(paste(
-    "\n--- Processando: ", "\nid:", id,
-    "\ncasa", casa, "\n"))
-  prop <- agoradigital::fetch_proposicao(id, casa)
-  emendas <- 
-    rcongresso::fetch_emendas(id, casa, prop$sigla_tipo, prop$numero, prop$ano)
+  emendas <- tibble::tibble()
+  if (casa == 'camara') {
+    emendas_relacionadas <- rcongresso::fetch_relacionadas(casa, id) %>% 
+      dplyr::filter(stringr::str_starts(siglaTipo,'E'))
+  }
+  
+  # cat(paste(
+  #   "\n--- Processando: ", "\nid:", id,
+  #   "\ncasa", casa, "\n"))
+  # prop <- agoradigital::fetch_proposicao(id, casa)
+  # emendas <- 
+  #   rcongresso::fetch_emendas(id, casa, prop$sigla_tipo, prop$numero, prop$ano)
   
   emendas
 }
