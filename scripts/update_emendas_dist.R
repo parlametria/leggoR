e6c64f81ab4422f9b58502b196eabc8aa3f9baca
Tarciso Braz <tarcisocomp@gmail.com>
2020-03-27 12:12:07 -0300

    Modifica script update_emendas_dist para que este verifique a existência de novas emendas antes de rodar o script de atualização das distâncias.

--- scripts/update_emendas_dist.R
+++ scripts/update_emendas_dist.R
@@ -18,15 +18,24 @@ distances_folderpath <- args[2]
 novas_emendas_filepath <- args[3]
 processed_emendas_filepath <- args[4]
 
+novas_emendas <- agoradigital::read_novas_emendas(novas_emendas_filepath)
+
+if (nrow(novas_emendas) == 0) {
+  cat("\nNão há novas emendas analisadas.\n")
+  quit()
+}
+
 dir.create(distances_folderpath, showWarnings = FALSE)
 
 ## Read emendas csv, add their distances and export the new emendas csv file
 files <- list.files(path = unformatted_distances_folderpath,pattern = "*.csv",
                     full.names = TRUE,
                     recursive = FALSE)
+
 emendas_distances_list <- purrr::map(files, ~ agoradigital::format_table_distances_to_emendas(
   distancias_datapath = .x, write_datapath = distances_folderpath))
-novas_emendas <- agoradigital::read_novas_emendas(novas_emendas_filepath)
+
 emendas_ja_analisadas <- agoradigital::read_emendas(processed_emendas_filepath)
+
 agoradigital::add_distances_to_emendas(novas_emendas, emendas_ja_analisadas, distances_folderpath) %>%
   readr::write_csv(processed_emendas_filepath)
