1a55ebc8e6709ab0d1afb01aa4413b0500951cfb
gileadekelvin <gileadekelvin@gmail.com>
2020-10-15 09:18:12 -0300

    Adiciona módulo para recuperação da informação de todos os relatores das proposições monitoradas

--- scripts/fetch_updated_bills_data.R
+++ scripts/fetch_updated_bills_data.R
@@ -10,6 +10,7 @@ flag = 2 Atualiza tudo referente as proposições
 flag = 3 Atualiza tudo sobre emendas
 flag = 4 Atualiza tudo sobre comissões
 flag = 5 Atualiza os dados de autores das matérias legislativas monitoradas
+flag = 6 Atualiza os dados de relatores das matérias legislativas monitoradas
 "
 
 #' @title Get arguments from command line option parsing
@@ -26,6 +27,11 @@ get_args <- function() {
     optparse::make_option(c("-p", "--pls_ids_filepath"),
                           type="character",
                           default="../inst/extdata/tabela_geral_ids_casa.csv",
+                          help=.HELP,
+                          metavar="character"),
+    optparse::make_option(c("-o", "--proposicoes_filepath"),
+                          type="character",
+                          default="../inst/extdata/proposicoes.csv",
                           help=.HELP,
                           metavar="character"),
     optparse::make_option(c("-e", "--exporth_path"),
@@ -45,6 +51,7 @@ args <- get_args()
 print(args)
 
 pls_ids_filepath <- args$pls_ids_filepath
+proposicoes_filepath <- args$proposicoes_filepath
 export_path <- args$exporth_path
 flag <- args$flag
 
@@ -81,11 +88,19 @@ export_comissoes <- function(export_path) {
   readr::write_csv(comissoes, paste0(export_path, "/comissoes.csv"))
 }
 
+#' @title Exporta dados de Relatorias
+#' @description Captura e escreve as relatorias
+#' @param export_path pasta para onde exportar dados.
+export_relatorias <- function(pls_ids_filepath, proposicoes_filepath, export_path) {
+  relatorias <- agoradigital::process_relatores_props(pls_ids_filepath, proposicoes_filepath, export_path)
+  readr::write_csv(relatorias, paste0(export_path, "/relatores_leggo.csv"))
+}
+
 #' @title Chama as funções corretas
 #' @description Recebe uma flag e chama as funções correspondetes
 #' @param flag Inteiro que representa qual função o usuário desejar chamar
 export_dados<- function(flag) {
-  if (!(flag %in% c(1, 2, 3, 4, 5))) {
+  if (!(flag %in% c(1, 2, 3, 4, 5, 6))) {
     stop(paste("Wrong flag!", .HELP, sep = "\n"))
   }else {
     ## Install local repository R package version
@@ -94,6 +109,7 @@ export_dados<- function(flag) {
       print("Atualizando tudo!")
       export_props(pls_ids_filepath, export_path)
       agoradigital::process_autores_props(pls_ids_filepath, export_path)
+      export_relatorias(pls_ids_filepath, proposicoes_filepath, export_path)
       export_emendas(pls_ids_filepath, export_path)
       export_comissoes(export_path)
     } else if (flag == 2) {
@@ -108,6 +124,9 @@ export_dados<- function(flag) {
     } else if (flag == 5) {
       print("Atualizando Autores das matérias legislativas!")
       agoradigital::process_autores_props(pls_ids_filepath, export_path)
+    } else if (flag == 6) {
+      print("Atualizando Relatores das matérias legislativas!")
+      export_relatorias(pls_ids_filepath, proposicoes_filepath, export_path)
     } else {
       stop(paste("Wrong flag!", .HELP, sep = "\n"))
     }
