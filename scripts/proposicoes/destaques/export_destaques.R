b253a246aea80d644c7d7015d1192454cc82fb2f
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-11-16 10:36:39 -0300

    Adiciona critério 1 - proposições que passaram pelo menos na primeira casa

--- scripts/proposicoes/destaques/export_destaques.R
+++ scripts/proposicoes/destaques/export_destaques.R
@@ -1,1 +1,54 @@
-#TODO: Adicionar export das proposições de destaque.
+library(here)
+print(dr_here())
+source(here::here("scripts/proposicoes/destaques/process_destaques.R"))
+
+.HELP <- "
+Usage:
+Rscript export_destaques.R -p <proposicoes_filepath> -t <progressos_filepath> -e <export_filepath>
+proposicoes_filepath: Caminho para o arquivos de proposições
+progressos_filepath: Caminho para o arquivos de progressos
+export_filepath: Caminho para o arquivo de destino.
+"
+
+#' @title Get arguments from command line option parsing
+#' @description Get arguments from command line option parsing
+get_args <- function() {
+  args = commandArgs(trailingOnly=TRUE)
+  
+  option_list = list(
+    optparse::make_option(c("-p", "--proposicoes_filepath"),
+                          type="character",
+                          default="../../../data/proposicoes.csv",
+                          help=.HELP,
+                          metavar="character"),
+    optparse::make_option(c("-t", "--progressos_filepath"),
+                          type="character",
+                          default="../../../data/progressos.csv",
+                          help=.HELP,
+                          metavar="character"),
+    optparse::make_option(c("-e", "--export_filepath"),
+                          type="character",
+                          default="../../../data/proposicoes_destaques.csv",
+                          help=.HELP,
+                          metavar="character")
+  );
+  
+  opt_parser <- optparse::OptionParser(option_list = option_list)
+  opt <- optparse::parse_args(opt_parser)
+  return(opt);
+}
+
+## Process args
+args <- get_args()
+print(args)
+
+proposicoes_filepath <- args$proposicoes_filepath
+progressos_filepath <- args$progressos_filepath
+saida <- args$export_filepath
+
+print("Processando proposições destaques")
+proposicoes_destaques <- process_proposicoes_destaques(proposicoes_filepath, progressos_filepath)
+
+print("Salvando proposições destaques...")
+readr::write_csv(proposicoes_destaques, saida)
+print("Salvo")
--- scripts/proposicoes/destaques/process_destaques.R
+++ scripts/proposicoes/destaques/process_destaques.R
@@ -1,1 +1,22 @@
-#TODO: Adicionar função(ões) que exporta(m) os dados das proposições de destaque.
+library(tidyverse)
+
+process_proposicoes_destaques <- function(
+  proposicoes_datapath = here::here("leggo_data/proposicoes.csv"),
+  progressos_datapath = here::here("leggo_data/progressos.csv")) {
+  
+  source(here::here("scripts/proposicoes/destaques/process_criterio_aprovada_em_uma_casa.R"))
+  
+  proposicoes_destaques <- read_csv(proposicoes_datapath) %>%
+    select(id_leggo, id_ext, casa)
+  
+  proposicoes_criterio_aprovada_em_uma_casa <-
+    process_criterio_aprovada_em_uma_casa(proposicoes_datapath,
+                                          progressos_datapath)
+  
+  proposicoes_destaques <- proposicoes_destaques %>%
+    inner_join(proposicoes_criterio_aprovada_em_uma_casa,
+               by = c("id_leggo", "id_ext", "casa"))
+  
+  return(proposicoes_destaques)
+  
+}
