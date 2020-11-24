0fc03a0e0e710baba0eb4de7929345a759ce1f0c
gileadekelvin <gileadekelvin@gmail.com>
2020-11-24 14:11:28 -0300

    Adiciona coluna com agenda para proposições em destaque

--- scripts/proposicoes/destaques/process_destaques.R
+++ scripts/proposicoes/destaques/process_destaques.R
@@ -2,28 +2,35 @@ library(tidyverse)
 
 process_proposicoes_destaques <- function(
   proposicoes_datapath = here::here("leggo_data/proposicoes.csv"),
-  progressos_datapath = here::here("leggo_data/progressos.csv")) {
+  progressos_datapath = here::here("leggo_data/progressos.csv"),
+  interesses_datapath = here::here("leggo_data/pls_interesses.csv")) {
   library(lubridate)
-  
+
   source(here::here("scripts/proposicoes/destaques/process_criterio_aprovada_em_uma_casa.R"))
-  
+
+  interesses <- read_csv(interesses_datapath) %>%
+    group_by(id_leggo) %>%
+    summarise(agendas = paste(interesse, collapse = ";")) %>%
+    ungroup()
+
   proposicoes_destaques <- read_csv(proposicoes_datapath) %>%
-    filter(status == "Ativa") %>% 
-    mutate(sigla = paste0(sigla_tipo, " ", numero, "/", year(ymd_hms(data_apresentacao)))) %>% 
-    select(id_leggo, id_ext, casa, sigla) %>% 
+    filter(status == "Ativa") %>%
+    mutate(sigla = paste0(sigla_tipo, " ", numero, "/", year(ymd_hms(data_apresentacao)))) %>%
+    select(id_leggo, id_ext, casa, sigla) %>%
     distinct()
-  
+
   proposicoes_criterio_aprovada_em_uma_casa <-
     process_criterio_aprovada_em_uma_casa(proposicoes_datapath,
-                                          progressos_datapath) %>% 
-    mutate(criterio_aprovada_em_uma_casa = T) %>% 
+                                          progressos_datapath) %>%
+    mutate(criterio_aprovada_em_uma_casa = T) %>%
     select(id_leggo, criterio_aprovada_em_uma_casa, fase_global, local, local_casa, data_inicio, data_fim)
-  
+
   proposicoes_destaques <- proposicoes_destaques %>%
     left_join(proposicoes_criterio_aprovada_em_uma_casa,
-               by = c("id_leggo")) %>% 
+               by = c("id_leggo")) %>%
+    left_join(interesses, by = c("id_leggo")) %>%
     mutate(criterio_aprovada_em_uma_casa = !is.na(criterio_aprovada_em_uma_casa))
-  
+
   return(proposicoes_destaques)
-  
+
 }
