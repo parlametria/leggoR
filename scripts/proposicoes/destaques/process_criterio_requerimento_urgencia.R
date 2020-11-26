9e1ec18785d40b288382f1add3067046a3893e06
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2020-11-26 12:12:07 -0300

    mudanças no regex'

--- scripts/proposicoes/destaques/process_criterio_requerimento_urgencia.R
+++ scripts/proposicoes/destaques/process_criterio_requerimento_urgencia.R
@@ -7,33 +7,40 @@
 
 process_criterio_requerimento_urgencia <- function(trams_datapath = here::here("leggo_data/trams.csv"),
                                                    props_datapath = here::here("leggo_data/proposicoes.csv") ){
-  trams <- read_csv(trams_datapath) %>% 
-    select(evento, id_ext, casa)
+  trams <- df %>%  
+    select(evento, id_ext, casa, data)
   
   props <- read_csv(props_datapath) %>%
     select(id_ext, casa, id_leggo)
   
   proposicoes_requerimento_urgencia <- trams %>% 
   left_join(props, by = c("id_ext", "casa")) %>%  
-  filter(evento == "requerimento_urgencia_aprovado", evento == "requerimento_urgencia_apresentado")
+  filter(str_detect(evento, "requerimento_urgencia_apresentado|requerimento_urgencia_aprovado"))
+  
+  proposicoes_sem_repetidos <- proposicoes_requerimento_urgencia %>% arrange(id_leggo, id_ext, casa, data) %>% 
+    distinct(id_leggo, id_ext, casa, .keep_all = TRUE)
+  
+  proposicoes_requerimento_urgencia_final <- proposicoes_sem_repetidos %>% spread(evento, data)
+  
+  return(proposicoes_requerimento_urgencia_final)
   
 }
 
 teste_csv_regex <- function(trams_datapath = here::here("leggo_data/trams.csv")){
-  tramitacoes <- read_csv(trams_datapath)
-  tramitacao_df <- tramitacoes
   
-  camara_env <- jsonlite::fromJSON(here::here("R/config/environment_senado.json"))
+  tramitacoes <- read_csv(trams_datapath)
+  # tramitacao_df <- tramitacoes
+  
+  camara_env <- jsonlite::fromJSON(here::here("R/config/environment_camara.json"))
   eventos_camara <- camara_env$eventos %>% dplyr::select(evento, regex)
-  df <- tramitacao_df %>%
+  df <- tramitacoes %>%
     dplyr::mutate(texto_lower = tolower(stringr::str_trim(
       stringr::str_replace_all(texto_tramitacao,'[\r\n]', '')))) %>%
-    mutate(evento_original = evento) %>%
+    # mutate(evento_original = evento) %>%
     fuzzyjoin::regex_left_join(eventos_camara, by = c(texto_lower = "regex")) %>%
     dplyr::select(-texto_lower, -regex)
   
-  df %>%
-    select(id_ext, texto_tramitacao, evento.x, evento.y) %>%
-    distinct(texto_tramitacao, .keep_all = T) %>%
+  df <- df %>%
+    mutate(evento = evento.y) %>% 
     view()
 }
