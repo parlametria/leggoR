615db10371dbadcce1c3e127675f62919bbf7a6a
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2020-11-27 17:07:07 -0300

    apagando testes

--- scripts/proposicoes/destaques/process_criterio_requerimento_urgencia.R
+++ scripts/proposicoes/destaques/process_criterio_requerimento_urgencia.R
@@ -28,22 +28,3 @@ process_criterio_requerimento_urgencia <- function(trams_datapath = here::here("
   return(proposicoes_requerimento_urgencia_final)
   
 }
-
-teste_csv_regex <- function(trams_datapath = here::here("leggo_data/trams.csv")){
-  
-  tramitacoes <- read_csv(trams_datapath)
-  # tramitacao_df <- tramitacoes
-  
-  camara_env <- jsonlite::fromJSON(here::here("R/config/environment_camara.json"))
-  eventos_camara <- camara_env$eventos %>% dplyr::select(evento, regex)
-  df <- tramitacoes %>%
-    dplyr::mutate(texto_lower = tolower(stringr::str_trim(
-      stringr::str_replace_all(texto_tramitacao,'[\r\n]', '')))) %>%
-    # mutate(evento_original = evento) %>%
-    fuzzyjoin::regex_left_join(eventos_camara, by = c(texto_lower = "regex")) %>%
-    dplyr::select(-texto_lower, -regex)
-  
-  df <- df %>%
-    mutate(evento = evento.y) %>% 
-    view()
-}
