f69affcb7a82ee19b1c064e5d230bd9c3264b0f0
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-12-23 13:33:46 -0300

    Finaliza processamento de requerimento audiência pública

--- scripts/proposicoes/destaques/process_criterio_requerimento_urgencia.R
+++ scripts/proposicoes/destaques/process_criterio_requerimento_urgencia.R
@@ -9,8 +9,17 @@
 process_criterio_requerimento_urgencia <- function(trams_datapath = here::here("leggo_data/trams.csv"),
                                                    props_datapath = here::here("leggo_data/proposicoes.csv") ){
   
-  trams <- read_csv(trams_datapath, col_types = cols(id_ext = col_character())) %>%  
+  trams <- read_csv(trams_datapath, col_types = cols(id_ext = col_character(),
+                                                     origem_tramitacao_local_nome_casa_local = col_character())) %>%  
     select(evento, id_ext, casa, data)
+  
+  # Filtra eventos dos últimos 4 anos 
+  hoje <- Sys.time()
+  trams <- trams %>%
+    mutate(idade = lubridate::interval(data, hoje) %>%
+             as.numeric('years')) %>%
+    dplyr::filter(idade <= 4) %>% 
+    select(-idade)
   
   props <- read_csv(props_datapath, col_types = cols(id_ext = col_character())) %>%
     select(id_ext, casa, id_leggo)
@@ -23,7 +32,39 @@ process_criterio_requerimento_urgencia <- function(trams_datapath = here::here("
   proposicoes_sem_repetidos <- proposicoes_requerimento_urgencia %>% arrange(id_leggo, id_ext, casa, data) %>% 
     distinct(id_leggo, id_ext, casa, .keep_all = TRUE)
   
-  proposicoes_requerimento_urgencia_final <- proposicoes_sem_repetidos %>% spread(evento, data)
+  proposicoes_requerimento_urgencia_final <-
+    proposicoes_sem_repetidos %>%
+    spread(evento, data) %>%
+    group_by(id_leggo) %>%
+    mutate(
+      casa_req_urgencia_apresentacao = if_else(
+        is.na(requerimento_urgencia_apresentado),
+        as.character(NA),
+        casa
+      ),
+      casa_req_urgencia_aprovacao = if_else(
+        is.na(requerimento_urgencia_aprovado),
+        as.character(NA),
+        casa
+      )
+    ) %>%
+    fill(requerimento_urgencia_apresentado, .direction = "downup") %>%
+    fill(requerimento_urgencia_aprovado, .direction = "downup") %>%
+    fill(casa_req_urgencia_apresentacao, .direction = "downup") %>%
+    fill(casa_req_urgencia_aprovacao, .direction = "downup") %>%
+    mutate(
+      requerimento_urgencia_apresentado = max(requerimento_urgencia_apresentado),
+      requerimento_urgencia_aprovado = max(requerimento_urgencia_aprovado)
+    ) %>%
+    ungroup() %>%
+    select(
+      id_leggo,
+      data_req_urgencia_apresentacao = requerimento_urgencia_apresentado,
+      data_req_urgencia_aprovacao = requerimento_urgencia_aprovado,
+      casa_req_urgencia_apresentacao,
+      casa_req_urgencia_aprovacao
+    ) %>%
+    distinct()
   
   return(proposicoes_requerimento_urgencia_final)
   
--- scripts/proposicoes/destaques/process_destaques.R
+++ scripts/proposicoes/destaques/process_destaques.R
@@ -56,9 +56,15 @@ process_proposicoes_destaques <- function(
  proposicoes_criterio_requerimento_urgencia <-
     process_criterio_requerimento_urgencia(tramitacoes_datapath,
                                       proposicoes_datapath) %>%
-    mutate(requerimento_urgencia_apresentado = ifelse(!is.na(requerimento_urgencia_apresentado), T , F),
-           requerimento_urgencia_aprovado = ifelse(!is.na(requerimento_urgencia_aprovado), T , F)) %>%
-    select(id_leggo, id_ext, casa, requerimento_urgencia_apresentado, requerimento_urgencia_aprovado)
+    mutate(criterio_req_urgencia_apresentado = !is.na(data_req_urgencia_apresentacao),
+           criterio_req_urgencia_aprovado = !is.na(data_req_urgencia_aprovacao)) %>%
+    select(id_leggo, 
+           criterio_req_urgencia_apresentado, 
+           criterio_req_urgencia_aprovado,
+           data_req_urgencia_apresentacao, 
+           data_req_urgencia_aprovacao,  
+           casa_req_urgencia_apresentacao, 
+           casa_req_urgencia_aprovacao)
 
   proposicoes_pressao_alta <-
     process_criterio_pressao_alta(pressao_datapath) %>%
@@ -84,11 +90,12 @@ process_proposicoes_destaques <- function(
     mutate(criterio_aprovada_em_uma_casa = !is.na(criterio_aprovada_em_uma_casa),
            criterio_avancou_comissoes = !is.na(criterio_avancou_comissoes),
            criterio_pressao_alta = !is.na(criterio_pressao_alta),
-           requerimento_urgencia_apresentado = !is.na(requerimento_urgencia_apresentado, requerimento_urgencia_aprovado),
+           criterio_req_urgencia_apresentado = if_else(is.na(criterio_req_urgencia_apresentado), FALSE, criterio_req_urgencia_apresentado),
+           criterio_req_urgencia_aprovado = if_else(is.na(criterio_req_urgencia_aprovado), FALSE, criterio_req_urgencia_aprovado),
            criterio_mais_comentadas_twitter = !is.na(criterio_mais_comentadas_twitter)) %>%
     left_join(interesses, by = c("id_leggo")) %>%
     distinct(id_leggo, .keep_all = TRUE) %>%
-    select(-c(id_ext, casa, sigla))
+    select(-c(id_ext))
 
   return(proposicoes_destaques)
 
