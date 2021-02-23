603db52e4f78b3cc59b908c47cb730fa827dd8a6
gileadekelvin <gileadekelvin@gmail.com>
2021-02-23 15:14:31 -0300

    Atualiza documentação da função de governismo

--- scripts/governismo/process_governismo.R
+++ scripts/governismo/process_governismo.R
@@ -6,10 +6,9 @@ library(pscl)
 #' @description Recupera informações de governimo para o parlametria usando o pacote perfilparlamentar
 #' @param votos_datapath Caminho para o csv de votos.
 #' Deve ter pelo menos 4 colunas: id_votacao, casa, id_parlamentar, voto.
-#' @return Dataframe com as PL's que serviram de entrada para o pipeline
-#' contendo id_camara,id_senado,apelido,tema,advocacy_link,keywords,tipo_agenda
+#' @return Dataframe de parlamentares e o governismo calculado.
 #' @example
-#' tabela_pls <- processa_lista_pls_interesses(url)
+#' governismo <- processa_governismo(votos_datapath)
 processa_governismo <- function(votos_datapath) {
   votos <- read_csv(votos_datapath,
                     col_types = cols(
