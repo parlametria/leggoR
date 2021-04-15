ee16bfec88190c1868058a3752a5b8021cdcab85
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-04-15 15:45:53 -0300

    Conserta pontos levantados em revisão

--- R/autores.R
+++ R/autores.R
@@ -1,3 +1,6 @@
+congresso_env <-
+  jsonlite::fromJSON(here::here("R/config/environment_congresso.json"))
+
 #' @title Recupera a casa da proposição que será usada para recuperar os seus autores
 #' @description A partir do id da proposição na Câmara e no Senado recupera qual a casa que será
 #' usada para recuperação dos autores.
@@ -110,6 +113,7 @@ process_autores_pl <-
         stop("Casa inválida!")
       }
     }, error = function(e) {
+      cat(paste0("Problemas ao baixar proposição de id", id_leggo, "\n"))
       message(e)
       return(tibble::tribble( ~ id_leggo, ~ id_ext, ~ id_autor, ~ tipo_autor, ~
                                 nome_autor))
@@ -124,14 +128,12 @@ process_autores_pl <-
 #' @param autores_filepath caminho para o dataframe de autores
 #' @export
 process_autores_props <- function(pls_ids_filepath, autores_filepath) {
-  set.seed(123)
   
   proposicoes_com_autores <- tibble::tibble(id_leggo = character())
   
   if (!is.null(autores_filepath) && file.exists(autores_filepath)) {
-    proposicoes_com_autores <- readr::read_csv(autores_filepath) %>% 
-      dplyr::distinct(id_leggo) %>% 
-      dplyr::pull(id_leggo)
+    proposicoes_com_autores <- readr::read_csv(autores_filepath, 
+                                               col_types = readr::cols("id_leggo" = "c"))
   }
   
   pls <- readr::read_csv(pls_ids_filepath, col_types = readr::cols(prioridade = "c")) %>%
@@ -140,7 +142,7 @@ process_autores_props <- function(pls_ids_filepath, autores_filepath) {
     dplyr::mutate(id_leggo = digest::digest(concat_chave_leggo, algo="md5", serialize=F)) %>%
     dplyr::select(-concat_chave_leggo) %>%
     dplyr::select(id_leggo, id_camara, id_senado) %>% 
-    dplyr::filter(!id_leggo %in% proposicoes_com_autores)
+    dplyr::filter(!id_leggo %in% proposicoes_com_autores$id_leggo)
   
   total_rows <- pls %>% nrow()
   
@@ -153,47 +155,60 @@ process_autores_props <- function(pls_ids_filepath, autores_filepath) {
       ~ process_autores_pl(..1, ..2, ..3, total_rows = total_rows)
     )
     
-    autores_leggo <- pls %>%
-      dplyr::select(id_leggo, id_camara, id_senado) %>%
-      dplyr::inner_join(
-        pls_autores %>%
-          select(id_leggo, id_autor, nome_autor, tipo_autor),
-        by = c("id_leggo")
-      ) %>%
-      fuzzyjoin::regex_left_join(
-        congresso_env$tipos_autores,
-        by = c(nome_autor = "regex"),
-        ignore_case = T
-      ) %>%
-      dplyr::mutate(id_autor = dplyr::if_else(!is.na(id_entidade),
-                                              as.character(id_entidade),
-                                              id_autor)) %>%
-      dplyr::select(-regex,-id_entidade) %>%
-      dplyr::mutate(
-        id_autor_parlametria = dplyr::case_when(
-          tipo_autor == "Deputado" ~ paste0(1, id_autor),
-          tipo_autor == "Senador" ~ paste0(2, id_autor),
-          tipo_autor == "Líder Senado" ~ paste0(2, id_autor),
-          TRUE ~ paste0(3, id_autor)
+    if (nrow(pls_autores) > 0) {
+      autores_leggo <- pls %>%
+        dplyr::select(id_leggo, id_camara, id_senado) %>%
+        dplyr::inner_join(
+          pls_autores %>%
+            select(id_leggo, id_autor, nome_autor, tipo_autor),
+          by = c("id_leggo")
+        ) %>%
+        fuzzyjoin::regex_left_join(
+          congresso_env$tipos_autores,
+          by = c(nome_autor = "regex"),
+          ignore_case = T
+        ) %>%
+        dplyr::mutate(id_autor = dplyr::if_else(!is.na(id_entidade),
+                                                as.character(id_entidade),
+                                                id_autor)) %>%
+        dplyr::select(-regex,-id_entidade) %>%
+        dplyr::mutate(
+          id_autor_parlametria = dplyr::case_when(
+            tipo_autor == "Deputado" ~ paste0(1, id_autor),
+            tipo_autor == "Senador" ~ paste0(2, id_autor),
+            tipo_autor == "Líder Senado" ~ paste0(2, id_autor),
+            TRUE ~ paste0(3, id_autor)
+          )
+        ) %>%
+        dplyr::select(id_leggo,
+                      id_camara,
+                      id_senado,
+                      id_autor_parlametria,
+                      id_autor)
+      
+      pls_sem_autores_baixados <- pls %>%
+        dplyr::filter(!id_leggo %in% (autores_leggo %>% dplyr::pull(id_leggo)))
+      
+      if (pls_sem_autores_baixados %>% nrow() > 0) {
+        message("Não foram recuperados os Autores para as seguintes proposições \n")
+        print(pls_sem_autores_baixados)
+      }
+      
+      autores <- proposicoes_com_autores %>%
+        dplyr::bind_rows(
+          autores_leggo %>%
+            dplyr::mutate(
+              id_camara = as.character(id_camara),
+              id_senado = as.character(id_senado),
+              id_autor = as.integer(id_autor),
+              id_autor_parlametria = as.integer(id_autor_parlametria)
+            )
         )
-      ) %>%
-      dplyr::select(id_leggo,
-                    id_camara,
-                    id_senado,
-                    id_autor_parlametria,
-                    id_autor)
-    
-    pls_sem_autores_baixados <- pls %>%
-      dplyr::filter(!id_leggo %in% (autores_leggo %>% dplyr::pull(id_leggo)))
-    
-    if (pls_sem_autores_baixados %>% nrow() > 0) {
-      message("Não foram recuperados os Autores para as seguintes proposições")
-      print(pls_sem_autores_baixados)
+      
+      readr::write_csv(autores,
+                       autores_filepath,
+                       na = "None")
     }
-    
-    readr::write_csv(autores_leggo,
-                     autores_filepath,
-                     na = "None")
   } else {
     cat("Não há novos autores de proposições.")
   }
