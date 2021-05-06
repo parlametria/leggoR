29429f9e77de8ff5a43618591681f899616e9063
João <joao.menezes@ccc.ufcg.edu.br>
2021-05-06 10:02:11 -0300

    comenta o metodo calcula_hora

--- scripts/utils-hora.R
+++ scripts/utils-hora.R
@@ -1,3 +1,8 @@
+#' @title Calcula hora
+#' @description Calcula oo tempo decorrido entre dois instantes de tempo
+#' @param time_init Instante de tempo inicial
+#' @param current_time Instante de tempo final
+#' @return Tempo decorrido em segundos/minutos
 calcula_hora <- function(time_init, current_time) {
   diff <- round(difftime(current_time, time_init, units = 'secs'), 3)
   if(diff > 60) {
