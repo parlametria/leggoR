2b355ca0ad5c9585910eef185594faf58f15dbd3
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2021-05-21 14:47:39 -0300

    mudanças na função do bot

--- scripts/logs/bot_parlametria.R
+++ scripts/logs/bot_parlametria.R
@@ -1,3 +1,9 @@
+#' @title Envia log para o bot
+#' @description Envia mensagem para o bot do Voz Ativa
+#' @param message_log mensagem que será enviada ao bot
+#' @return Mensagem indicando que a mensagem foi enviada com sucesso
+#' @examples
+#' send_log_to_bot('processamento completo')
 send_log_to_bot <- function(message_log) {
   library(httr)
   
