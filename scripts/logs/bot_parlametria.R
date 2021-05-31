4a582251576a318d65f2b65baad096620aa45d10
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2021-05-31 15:30:04 -0300

    adicionando função de log e strings

--- scripts/logs/bot_parlametria.R
+++ scripts/logs/bot_parlametria.R
@@ -17,3 +17,28 @@ send_log_to_bot <- function(message_log) {
   
   return("Mensagem Enviada!")
 }
+
+#' @title Cria log de mensagem das apensadas
+#' @description Cria mensagem das apensadas que será enviado do bot
+#' @param props_apensadas_nao_monitoradas_filepath caminho para csv das props apensadas
+#' @return Mensagem de log
+#' @examples
+#' create_log('props_apensadas_nao_monitoradas.csv')
+create_log <- function(props_apensadas_nao_monitoradas_filepath){
+  
+  props <- read.csv(props_apensadas_nao_monitoradas_filepath)
+  
+  df <- props %>% 
+    mutate(original = id_ext_prop_principal_raiz) %>% 
+    mutate(apensada=id_ext_prop_principal) %>% 
+    mutate(log = paste0("casa: ", casa, 
+                        " original: ", original, 
+                        " apensada: ", apensada, 
+                        " interesse: ", interesse)) %>% 
+    select(log)
+  
+  string_log <- paste0(df$log, collapse = "\n ")
+  
+  return (string_log)
+  
+}
